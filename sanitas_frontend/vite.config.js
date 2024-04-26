ef000915effb7c40eab5650b3cfcb99e4af645a9
XavierLopez25 <lop22716@uvg.edu.gt>
2024-04-25 23:36:44 -0600

    feat: added setupTests.js file for tests config

--- sanitas_frontend/vite.config.js
+++ sanitas_frontend/vite.config.js
@@ -4,4 +4,8 @@ import { defineConfig } from "vite";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  test: {
+    environment: "jsdom",
+    setupFiles: "./src/utils/setupTests.js",
+  },
 });
