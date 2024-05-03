2e5ee021d2e16cbc6a94d915ac1f75507b96b01d
DanielDubon <dub22233@uvg.edu.gt>
2024-05-03 02:36:19 -0600

    fix: vite workflow deployment path

--- sanitas_frontend/vite.config.js
+++ sanitas_frontend/vite.config.js
@@ -4,6 +4,7 @@ import { defineConfig } from "vite";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/Sanitas/',
   test: {
     environment: "jsdom",
     setupFiles: "./src/__test__/ui/UITestSetup.js",
