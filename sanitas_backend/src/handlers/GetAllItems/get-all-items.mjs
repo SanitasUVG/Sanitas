9e1e33d8d3ce51a0e81c156f29d841239e567bfb
ElrohirGT <elrohirgt@gmail.com>
2024-05-12 09:52:08 -0600

    feat: Removed Xata from the project and added a parameter to template.yml

--- sanitas_backend/src/handlers/GetAllItems/get-all-items.mjs
+++ sanitas_backend/src/handlers/GetAllItems/get-all-items.mjs
@@ -1,49 +1,37 @@
-// Create clients and set shared const values outside of the handler.
-import pino from "pino";
-import { lambdaRequestTracker, pinoLambdaDestination } from "pino-lambda";
-
-// custom destination formatter
-const destination = pinoLambdaDestination();
-const logger = pino(
-  {
-    // typical pino options
-  },
-  destination,
-);
-const withRequest = lambdaRequestTracker();
-
-import { getXataClient } from "db-conn";
+import { logger, withRequest } from "logging";
+import { getPgClient } from "db-conn";
 
 /**
- * A simple example includes a HTTP get method to get all items from a DynamoDB table.
+ * A simple example includes a HTTP get method.
  */
 export const getAllItemsHandler = async (event, context) => {
   // All log statements are written to CloudWatch
   withRequest(event, context);
   if (event.httpMethod !== "GET") {
     throw new Error(
-      `getAllItems only accept GET method, you tried: ${event.httpMethod}`,
+      `getAllItems only accepts GET method, you tried: ${event.httpMethod}`,
     );
   }
 
-  const client = getXataClient(
-    process.env.XATA_API_KEY,
-    process.env.XATA_BRANCH,
-  );
-  const pacientes = await client.db.Paciente.getPaginated({
-    pagination: { size: 5, offset: 0 },
-  });
-  let records = pacientes.records;
+  logger.info(process.env, "Las variables de entorno son:");
 
-  logger.info(records, "Los primeros 5 pacientes son:");
+  try {
+    logger.info("Connecting to DB...");
+    const client = getPgClient(process.env.POSTGRES_URL);
 
-  const secondPage = await pacientes.nextPage();
-  logger.info(secondPage.records, "La segunda página con 5 pacientes es:");
+    logger.info("Querying DB...");
+    const response = await client.query("SELECT 2+2;");
+    logger.info(response.rows, "The response from the DB is:");
+  } catch (error) {
+    logger.error(error, "An error has ocurred!");
+  }
 
+  logger.info("Creating response...");
   const response = {
     statusCode: 200,
-    body: JSON.stringify(secondPage.records),
+    body: JSON.stringify([]),
   };
 
+  logger.info(response, "Responding with:");
   return response;
 };
