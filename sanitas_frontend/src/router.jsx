15936964def6e6cd5794d09ebc8da685f222d6d3
ElrohirGT <elrohirgt@gmail.com>
2024-05-24 00:39:49 -0600

    chore: formatting

--- sanitas_frontend/src/router.jsx
+++ sanitas_frontend/src/router.jsx
@@ -7,22 +7,22 @@ import UpdateInfoView from "./views/UpdateInfoView";
 const useStore = createEmptyStore();
 
 export const NAV_PATHS = {
-	SEARCH_PATIENT: import.meta.env.BASE_URL,
-	ADD_PATIENT: `${import.meta.env.BASE_URL}form-patient`,
-	UPDATE_PATIENT: `${import.meta.env.BASE_URL}update-view`,
+  SEARCH_PATIENT: import.meta.env.BASE_URL,
+  ADD_PATIENT: `${import.meta.env.BASE_URL}form-patient`,
+  UPDATE_PATIENT: `${import.meta.env.BASE_URL}update-view`,
 };
 
 export const ROUTES = [
-	{
-		path: NAV_PATHS.SEARCH_PATIENT,
-		element: <SearchPatientView searchPatientsApiCall={searchPatient} useStore={useStore} />,
-	},
-	{
-		path: NAV_PATHS.ADD_PATIENT,
-		element: <AddPatientView checkCui={checkCui} submitPatientData={submitPatientData} />,
-	},
-	{
-		path: NAV_PATHS.UPDATE_PATIENT,
-		element: <UpdateInfoView />,
-	},
+  {
+    path: NAV_PATHS.SEARCH_PATIENT,
+    element: <SearchPatientView searchPatientsApiCall={searchPatient} useStore={useStore} />,
+  },
+  {
+    path: NAV_PATHS.ADD_PATIENT,
+    element: <AddPatientView checkCui={checkCui} submitPatientData={submitPatientData} />,
+  },
+  {
+    path: NAV_PATHS.UPDATE_PATIENT,
+    element: <UpdateInfoView />,
+  },
 ];
