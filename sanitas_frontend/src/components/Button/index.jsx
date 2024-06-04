e80c8b42cc36686c3857fe92a2c5adc2d6fa9190
Nahomy Castro <98111102+lemoonchild@users.noreply.github.com>
2024-06-04 12:01:45 -0600

    feat: added reusable components (#80)

--- sanitas_frontend/src/components/Button/index.jsx
+++ sanitas_frontend/src/components/Button/index.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Button({ text, onClick }) {
   return (
-    <button onClick={onClick} type="button">
+    <button type="button" onClick={onClick}>
       {text}
     </button>
   );
---
+++ sanitas_frontend/src/components/DropdownMenu/index.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function DropdownMenu({ value, onChange, options }) {
+  return (
+    <select value={value} onChange={onChange}>
+      {options.map((option) => (
+        <option key={option.value} value={option.value}>
+          {option.label}
+        </option>
+      ))}
+    </select>
+  );
+}
