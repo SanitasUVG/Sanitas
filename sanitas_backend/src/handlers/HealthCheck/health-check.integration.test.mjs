5e359ddc7c874082fa6feb0cb596149ea8af184e
DanielDubon <dub22233@uvg.edu.gt>
2024-06-03 21:41:46 -0600

    feat: health check endpoint added

---
+++ sanitas_backend/src/handlers/HealthCheck/health-check.integration.test.mjs
@@ -0,0 +1,20 @@
+import { afterAll, beforeAll, describe, expect, test } from "@jest/globals";
+import axios from "axios";
+const LOCAL_API_URL = "http://localhost:3000/health-check";
+
+describe("Health Check Integration Test", () => {
+  beforeAll(() => {
+    // Insert data into DB.
+  });
+  afterAll(() => {
+    // Delete inserted data.
+  });
+
+  test("should return DB status: UP if the database connection is successful", async () => {
+    const response = await axios.get(LOCAL_API_URL);
+    expect(response).toBeDefined();
+    expect(response.status).toBe(200);
+    expect(response.data).toBeDefined();
+    expect(response.data.status).toBe("UP");
+  });
+});
--- sanitas_backend/src/handlers/HealthCheck/health-check.mjs
+++ sanitas_backend/src/handlers/HealthCheck/health-check.mjs
@@ -6,26 +6,21 @@ export const handler = async (event, context) => {
 
   if (event.httpMethod !== "GET") {
     throw new Error(
-      `Check Cui solo acepta el método GET, intentaste: ${event.httpMethod}`,
+      `Health Check solo acepta el método GET, intentaste: ${event.httpMethod}`,
     );
   }
 
-  const cui = event.pathParameters.cui;
   logger.info(process.env, "Las variables de entorno son:");
   let client;
+
   try {
     const url = process.env.POSTGRES_URL;
     client = getPgClient(url);
     logger.info(url, "Connecting to DB...");
     await client.connect();
     logger.info("Querying DB...");
-
-    const query = "SELECT COUNT(*) FROM PACIENTE WHERE cui = $1";
-    const values = [cui];
-    const response = await client.query(query, values);
-    logger.info(response.rows, "The response from the DB is:");
-    const exists = response.rows[0].count > 0;
-
+    const query = "SELECT * FROM PACIENTE";
+    await client.query(query);
     await client.end();
 
     return {
@@ -35,14 +30,21 @@ export const handler = async (event, context) => {
         "Access-Control-Allow-Origin": "*", // Allow from anywhere
         "Access-Control-Allow-Methods": "GET", // Allow only GET request
       },
-      body: JSON.stringify({ exists }),
+      body: JSON.stringify({
+        name: "DB",
+        status: "UP",
+      }),
     };
   } catch (error) {
     logger.error(error, "Error querying database:");
-    await client.end();
     return {
-      statusCode: 500,
-      body: JSON.stringify({ error: "Internal Server Error" }),
+      statusCode: 200,
+      headers: {
+        "Access-Control-Allow-Headers": "Content-Type",
+        "Access-Control-Allow-Origin": "*", // Allow from anywhere
+        "Access-Control-Allow-Methods": "GET", // Allow only GET request
+      },
+      body: JSON.stringify({ name: "DB", status: "DOWN", error: error.message }),
     };
   }
 };
