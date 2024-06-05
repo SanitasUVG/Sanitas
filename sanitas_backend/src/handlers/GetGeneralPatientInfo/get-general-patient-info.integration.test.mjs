47dcfd8006a1b312b30ec69cb233b79ceb3520bf
ElrohirGT <elrohirgt@gmail.com>
2024-06-04 20:34:40 -0600

    feat: Implemented /patient/general/{id} endpoint

--- sanitas_backend/src/handlers/GetGeneralPatientInfo/get-general-patient-info.integration.test.mjs
+++ sanitas_backend/src/handlers/GetGeneralPatientInfo/get-general-patient-info.integration.test.mjs
@@ -1,7 +1,7 @@
 import { afterAll, beforeAll, describe, expect, test } from "@jest/globals";
 import axios from "axios";
 
-const LOCAL_API_URL = "http://localhost:3000/patient/";
+const LOCAL_API_URL = "http://localhost:3000/patient/general/";
 
 describe("Get patient integration tests", () => {
   test("Get patient that exists", async () => {
@@ -10,14 +10,14 @@ describe("Get patient integration tests", () => {
     expect(response).toBeDefined();
     expect(response.status).toBe(200);
 
-    const user = JSON.parse(response.data);
+    const user = response.data;
     expect(user).toBeDefined();
     expect(user.id).toBe(1);
     expect(user.cui).toBe("1234567890123");
     expect(user.email).toBe("juan.perez@example.com");
     expect(user.isWoman).toBe(false);
     expect(user.names).toBe("Juan");
-    expect(user.lastnames).toBe("Pérez");
+    expect(user.lastNames).toBe("Pérez");
 
     expect(user.contactName1).toBe("Maria Pérez");
     expect(user.contactKinship1).toBe("Madre");
@@ -29,8 +29,9 @@ describe("Get patient integration tests", () => {
 
     expect(user.bloodType).toBe("O+");
     expect(user.address).toBe("Calle Falsa 123, Ciudad");
-    expect(user.birthdate).toBe("1990-01-01");
+    expect(user.birthdate).toBe("1990-01-01T00:00:00.000Z");
     expect(user.phone).toBe("1234567890");
+    expect(user.insuranceId).toBe(1);
   });
 
   test("Fail when retrieving patient that doesn't exists", async () => {
@@ -40,7 +41,7 @@ describe("Get patient integration tests", () => {
     expect(response).toBeDefined();
     expect(response.status).toBe(400);
 
-    const { message } = JSON.parse(response.data);
+    const { message } = response.data;
     expect(message).toBe("Invalid request: No patient with the given id found.");
   });
 });
