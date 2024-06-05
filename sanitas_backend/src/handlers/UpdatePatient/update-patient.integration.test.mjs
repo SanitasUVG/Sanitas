dd4dfcd526e666ce2f40868c51500f6357715952
Bianca <cal22272@uvg.edu.gt>
2024-06-05 17:54:43 -0600

    fix: updated endpoint

--- sanitas_backend/src/handlers/UpdatePatient/update-patient.integration.test.mjs
+++ sanitas_backend/src/handlers/UpdatePatient/update-patient.integration.test.mjs
@@ -3,7 +3,7 @@ import axios from "axios";
 
 const LOCAL_API_URL = "http://localhost:3000/";
 
-describe("Update Patient Integration Tests", () => {
+describe("Update Patient integration tests", () => {
   beforeAll(() => {
     // Insert data into DB.
   });
@@ -12,27 +12,45 @@ describe("Update Patient Integration Tests", () => {
     // Delete data into DB.
   });
 
-  test("Normal case: Update patient data", async () => {
+  test("Normal case: Actualizar datos de un paciente existente", async () => {
     const patientData = {
       cui: "1234567890123",
-      nombres: "Juan",
-      apellidos: "Pérez",
-      telefono: "1234567890", // updating the phone number
+      names: "Juan Actualizado",
+      lastNames: "Pérez Actualizado",
+      phone: "5556789",
     };
-    const response = await axios.post(`${LOCAL_API_URL}/patient/update`, patientData);
+
+    const response = await axios.put(`${LOCAL_API_URL}/patient/general`, patientData);
 
     expect(response).toBeDefined();
     expect(response.status).toBe(200);
+    expect(response.data.message).toBe("Datos del paciente actualizados exitosamente.");
   });
 
-  test("Update patient data without any fields (should fail)", async () => {
-    const patientData = {}; // Empty object, should fail as no fields provided for update
+  test("Actualizar datos de un paciente sin proporcionar ningún campo para actualizar", async () => {
+    const patientData = {
+      cui: "1234567890123",
+    };
 
-    const response = await axios.post(`${LOCAL_API_URL}/patient/update`, patientData, {
-      validateStatus: () => true,
+    const response = await axios.put(`${LOCAL_API_URL}/patient/general`, patientData);
+
+    expect(response).toBeDefined();
+    expect(response.status).toBe(200);
+    expect(response.data.message).toBe("Datos del paciente actualizados exitosamente.");
+  });
+
+  test("Actualizar datos de un paciente con CUI inexistente (debería fallar)", async () => {
+    const patientData = {
+      cui: "9876543210987",
+      names: "Nombre Nuevo",
+    };
+
+    const response = await axios.put(`${LOCAL_API_URL}/patient/general`, patientData, {
+      validateStatus: () => true, // Para que axios no lance un error en caso de status >= 400
     });
 
-    expect(response.status).toBe(422); // Expecting Unprocessable Entity status
-    expect(response.data.error).toBe("At least one field is required for update.");
+    // Verificar que el error sea el esperado
+    expect(response.status).toBe(400);
+    expect(response.data.error).toBe("No se encontraron registros con el CUI proporcionado.");
   });
 });
--- sanitas_backend/src/handlers/UpdatePatient/update-patient.mjs
+++ sanitas_backend/src/handlers/UpdatePatient/update-patient.mjs
@@ -1,57 +1,176 @@
 import { getPgClient } from "db-conn";
 import { logger, withRequest } from "logging";
 
+// Funciones de mapeo
+function mapToAPIPatient(dbPatient) {
+  const {
+    id,
+    cui,
+    es_mujer: isWoman,
+    correo: email,
+    nombres: firstName,
+    apellidos: lastName,
+
+    nombre_contacto1: contactName1,
+    parentesco_contacto1: contactKinship1,
+    telefono_contacto1: contactPhone1,
+
+    nombre_contacto2: contactName2,
+    parentesco_contacto2: contactKinship2,
+    telefono_contacto2: contactPhone2,
+
+    tipo_sangre: bloodType,
+    direccion: address,
+    id_seguro: insuranceId,
+    fecha_nacimiento: birthDate,
+    telefono: phone,
+  } = dbPatient;
+
+  return {
+    id,
+    cui,
+    isWoman,
+    email,
+    firstName,
+    lastName,
+    contactName1,
+    contactKinship1,
+    contactPhone1,
+    contactName2,
+    contactKinship2,
+    contactPhone2,
+    bloodType,
+    address,
+    insuranceId,
+    birthDate,
+    phone,
+  };
+}
+
+function mapToDbPatient(apiPatient) {
+  const {
+    id,
+    cui,
+    isWoman: es_mujer,
+    email: correo,
+    firstName: nombres,
+    lastName: apellidos,
+
+    contactName1: nombre_contacto1,
+    contactKinship1: parentesco_contacto1,
+    contactPhone1: telefono_contacto1,
+
+    contactName2: nombre_contacto2,
+    contactKinship2: parentesco_contacto2,
+    contactPhone2: telefono_contacto2,
+
+    bloodType: tipo_sangre,
+    address: direccion,
+    insuranceId: id_seguro,
+    birthDate: fecha_nacimiento,
+    phone: telefono,
+  } = apiPatient;
+
+  return {
+    id,
+    cui,
+    es_mujer,
+    correo,
+    nombres,
+    apellidos,
+    nombre_contacto1,
+    parentesco_contacto1,
+    telefono_contacto1,
+    nombre_contacto2,
+    parentesco_contacto2,
+    telefono_contacto2,
+    tipo_sangre,
+    direccion,
+    id_seguro,
+    fecha_nacimiento,
+    telefono,
+  };
+}
+
 export const updatePatientHandler = async (event, context) => {
   withRequest(event, context);
 
-  if (event.httpMethod !== "POST") {
-    throw new Error(`updatePatientHandler only accepts the POST method, you tried: ${event.httpMethod}`);
+  if (event.httpMethod !== "PUT") {
+    throw new Error(`updatePatientHandler solo acepta el método PUT, intentaste: ${event.httpMethod}`);
   }
 
-  const patientData = JSON.parse(event.body);
+  const apiPatientData = JSON.parse(event.body);
+  const patientData = mapToDbPatient(apiPatientData);
 
-  // Logging environment variables
-  logger.info(process.env, "Environment variables:");
+  logger.info(process.env, "Las variables de entorno son:");
 
   let client;
   try {
     const url = process.env.POSTGRES_URL;
-    logger.info(url, "Connecting to the database...");
+    logger.info(url, "Conectando a la base de datos...");
     client = getPgClient(url);
     await client.connect();
 
-    logger.info(patientData, "Updating patient data in the database...");
+    logger.info(patientData, "Actualizando datos del paciente en la base de datos...");
 
-    // Validating that at least one field is provided for update
-    if (Object.keys(patientData).length === 0) {
-      throw new Error("No fields provided for update.");
+    if (!patientData.cui) {
+      throw new Error("CUI es requerido.");
     }
 
-    // Constructing the update query dynamically based on provided fields
-    let query = "UPDATE PACIENTE SET ";
-    const values = [];
-    let index = 1;
-    for (const key in patientData) {
-      query += `${key.toUpperCase()} = $${index}, `;
-      values.push(patientData[key]);
-      index++;
-    }
-    // Removing the trailing comma and space
-    query = query.slice(0, -2);
-    query += ` WHERE CUI = $${index}`;
-    values.push(patientData.cui);
+    const query = `
+      UPDATE paciente
+      SET 
+        nombres = COALESCE($2, nombres),
+        apellidos = COALESCE($3, apellidos),
+        nombre_contacto1 = COALESCE($4, nombre_contacto1),
+        parentesco_contacto1 = COALESCE($5, parentesco_contacto1),
+        telefono_contacto1 = COALESCE($6, telefono_contacto1),
+        nombre_contacto2 = COALESCE($7, nombre_contacto2),
+        parentesco_contacto2 = COALESCE($8, parentesco_contacto2),
+        telefono_contacto2 = COALESCE($9, telefono_contacto2),
+        tipo_sangre = COALESCE($10, tipo_sangre),
+        direccion = COALESCE($11, direccion),
+        id_seguro = COALESCE($12, id_seguro),
+        fecha_nacimiento = COALESCE($13, fecha_nacimiento),
+        telefono = COALESCE($14, telefono)
+      WHERE cui = $1
+    `;
+    const values = [
+      patientData.cui,
+      patientData.nombres || null,
+      patientData.apellidos || null,
+      patientData.nombre_contacto1 || null,
+      patientData.parentesco_contacto1 || null,
+      patientData.telefono_contacto1 || null,
+      patientData.nombre_contacto2 || null,
+      patientData.parentesco_contacto2 || null,
+      patientData.telefono_contacto2 || null,
+      patientData.tipo_sangre || null,
+      patientData.direccion || null,
+      patientData.id_seguro || null,
+      patientData.fecha_nacimiento ? new Date(patientData.fecha_nacimiento) : null,
+      patientData.telefono || null,
+    ];
 
-    await client.query(query, values);
-    logger.info("Patient data updated successfully.");
+    logger.info({ query, values }, "Consulta SQL y valores:");
+
+    const result = await client.query(query, values);
+
+    if (result.rowCount === 0) {
+      throw new Error("No se encontraron registros con el CUI proporcionado.");
+    }
+
+    logger.info("Datos del paciente actualizados exitosamente.");
   } catch (error) {
-    logger.error(error, "An error occurred while updating patient data.");
+    logger.error(error, "¡Se produjo un error al actualizar los datos del paciente!");
 
     let statusCode = 400;
-    let errorMessage = "An error occurred while updating patient data.";
+    let errorMessage = "Se produjo un error al actualizar los datos del paciente.";
 
-    if (error.message === "No fields provided for update.") {
-      statusCode = 422; // Unprocessable Entity
-      errorMessage = "At least one field is required for update.";
+    if (error.message === "CUI es requerido.") {
+      errorMessage = "CUI es requerido.";
+    } else if (error.message === "No se encontraron registros con el CUI proporcionado.") {
+      errorMessage = "No se encontraron registros con el CUI proporcionado.";
     }
 
     const response = {
@@ -68,11 +187,11 @@ export const updatePatientHandler = async (event, context) => {
     headers: {
       "Access-Control-Allow-Headers": "Content-Type",
       "Access-Control-Allow-Origin": "*", // Allow from anywhere
-      "Access-Control-Allow-Methods": "POST", // Allow only POST request
+      "Access-Control-Allow-Methods": "PUT", // Allow only PUT request
     },
-    body: JSON.stringify({ message: "Patient data updated successfully." }),
+    body: JSON.stringify({ message: "Datos del paciente actualizados exitosamente." }),
   };
 
-  logger.info(response, "Responding with:");
+  logger.info(response, "Respondiendo con:");
   return response;
 };
