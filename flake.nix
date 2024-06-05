9e28e1a03493d8aeb0f7c678724bc479fa0904b2
ElrohirGT <elrohirgt@gmail.com>
2024-06-04 18:38:54 -0600

    fix: Starting services on a Mac

--- flake.nix
+++ flake.nix
@@ -108,7 +108,12 @@
                 #   ready_log_line = "for react-vite started";
                 # };
               };
-              backend.exec = "cd sanitas_backend/ && sam build && sam local start-api --add-host=hostpc:$(ip route get 1.2.3.4 | awk '{print $7}')";
+              backend.exec = let
+                ipCommand =
+                  if builtins.elem system ["x86_64-darwin" "aarch64-darwin"]
+                  then "ifconfig en0 | grep 'inet ' | awk '{print $2}'"
+                  else "ip route get 1.2.3.4 | awk '{print $7}'";
+              in "cd sanitas_backend/ && sam build && sam local start-api --add-host=hostpc:$(${ipCommand})";
               pg_setup = {
                 exec = "cat pg_hba.conf > ./.devenv/state/postgres/pg_hba.conf";
                 process-compose = {
