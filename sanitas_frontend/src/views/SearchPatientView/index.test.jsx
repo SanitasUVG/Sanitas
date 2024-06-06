6d99a9d3acadb0b6be576627310dbaa0f4186326
XavierLopez25 <lop22716@uvg.edu.gt>
2024-06-06 14:34:21 -0600

    fix: changed test value due to changes in the input component value handling

--- sanitas_frontend/src/views/SearchPatientView/index.test.jsx
+++ sanitas_frontend/src/views/SearchPatientView/index.test.jsx
@@ -21,7 +21,7 @@ describe("Search Patient view ui tests", () => {
     expect(apiCall).toHaveBeenCalledTimes(0);
   });
 
-  test("On search calls function", () => {
+  test("On search calls function", async () => {
     const apiCall = vi.fn(() => ({
       result: [],
     }));
@@ -36,10 +36,13 @@ describe("Search Patient view ui tests", () => {
     const searchElem = dom.getByPlaceholderText("Ingrese su búsqueda...");
     const searchBtn = dom.getByText("Buscar");
 
-    fireEvent.change(searchElem, { target: { value: "asdflkjlk" } });
+    fireEvent.change(searchElem, { target: { value: "3284834428" } });
     fireEvent.click(searchBtn);
 
-    expect(apiCall).toHaveBeenCalledOnce();
+    // Use waitFor to ensure the async call has time to complete
+    await waitFor(() => {
+      expect(apiCall).toHaveBeenCalledOnce();
+    });
   });
 
   test("Display a button to see patient", async () => {
@@ -62,16 +65,19 @@ describe("Search Patient view ui tests", () => {
     const searchElem = dom.getByPlaceholderText("Ingrese su búsqueda...");
     const searchBtn = dom.getByText("Buscar");
 
-    fireEvent.change(searchElem, { target: { value: "asdflkj" } });
+    fireEvent.change(searchElem, { target: { value: "2348234890" } });
     fireEvent.click(searchBtn);
 
     expect(apiCall).toHaveBeenCalledOnce();
 
     // The function below throws if 0 or 2+ elements are found.
-    await waitFor(() => {
-      expect(dom.getByText("Ver")).toBeVisible();
-    }, {
-      timeout: 500,
-    });
+    await waitFor(
+      () => {
+        expect(dom.getByText("Ver")).toBeVisible();
+      },
+      {
+        timeout: 500,
+      },
+    );
   });
 });
