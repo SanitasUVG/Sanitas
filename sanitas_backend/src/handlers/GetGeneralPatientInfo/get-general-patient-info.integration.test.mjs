fd08feb1ea50b6053b0a9efd426d47f9af555162
DanielDubon <100601875+DanielDubon@users.noreply.github.com>
2024-06-06 08:53:26 -0600

    feat: update patient GUI added (#89)
    * feat: update patient GUI added
    
    * feat: storybook and tests were added
    
    * fix: storybook properties names were fixed
    
    * fix: integration tests with helpers (#91)
    
    * feat: Added test helpers
    
    * feat: Added JSDoc configuration and dependency
    
    ---------
    
    Co-authored-by: ElrohirGT <45268815+ElrohirGT@users.noreply.github.com>

--- sanitas_backend/src/handlers/GetGeneralPatientInfo/get-general-patient-info.integration.test.mjs
+++ sanitas_backend/src/handlers/GetGeneralPatientInfo/get-general-patient-info.integration.test.mjs
@@ -1,42 +1,36 @@
-import { afterAll, beforeAll, describe, expect, test } from "@jest/globals";
+import { beforeAll, describe, expect, test } from "@jest/globals";
 import axios from "axios";
+import { createTestPatient, generateUniqueCUI, LOCAL_API_URL } from "../testHelpers.mjs";
 
-const LOCAL_API_URL = "http://localhost:3000/patient/general/";
+const API_URL = `${LOCAL_API_URL}patient/general/`;
 
 describe("Get patient integration tests", () => {
+  let cui;
+  let patientId;
+
+  beforeAll(async () => {
+    cui = generateUniqueCUI();
+    patientId = await createTestPatient(cui);
+  });
+
   test("Get patient that exists", async () => {
-    const response = await axios.get(LOCAL_API_URL + "1");
+    const response = await axios.get(API_URL + patientId);
 
     expect(response).toBeDefined();
     expect(response.status).toBe(200);
 
     const user = response.data;
     expect(user).toBeDefined();
-    expect(user.id).toBe(1);
-    expect(user.cui).toBe("1234567890123");
-    expect(user.email).toBe("juan.perez@example.com");
+    expect(user.id).toBe(patientId);
+    expect(user.cui).toBe(cui);
     expect(user.isWoman).toBe(false);
-    expect(user.names).toBe("Juan");
-    expect(user.lastNames).toBe("Pérez");
-
-    expect(user.contactName1).toBe("Maria Pérez");
-    expect(user.contactKinship1).toBe("Madre");
-    expect(user.contactPhone1).toBe("987654321");
-
-    expect(user.contactName2).toBe("Jose Pérez");
-    expect(user.contactKinship2).toBe("Padre");
-    expect(user.contactPhone2).toBe("987650123");
-
-    expect(user.bloodType).toBe("O+");
-    expect(user.address).toBe("Calle Falsa 123, Ciudad");
-    expect(user.birthdate).toBe("1990-01-01T00:00:00.000Z");
-    expect(user.phone).toBe("1234567890");
-    expect(user.insuranceId).toBe(1);
+    expect(user.names).toBe("Flabio André");
+    expect(user.lastNames).toBe("Galán Dona");
   });
 
   test("Fail when retrieving patient that doesn't exists", async () => {
     // Para que axios no lance un error en caso de status >= 400
-    const response = await axios.get(LOCAL_API_URL + "10000", { validateStatus: () => true });
+    const response = await axios.get(API_URL + "999123999", { validateStatus: () => true });
 
     expect(response).toBeDefined();
     expect(response.status).toBe(400);
