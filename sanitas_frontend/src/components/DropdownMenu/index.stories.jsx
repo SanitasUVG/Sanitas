ebd2ce6b2e36f5e5b6280baedd3e9438366f8885
lemoonchild <ncastrom21@gmail.com>
2024-06-06 17:46:30 -0600

    feat: added styles and storybook to dropdown menu

--- sanitas_frontend/src/components/DropdownMenu/index.stories.jsx
+++ sanitas_frontend/src/components/DropdownMenu/index.stories.jsx
@@ -13,7 +13,7 @@ Default.args = {
   value: "option1",
   onChange: () => {},
   options: [
-    { value: "option1", label: "O+" },
+    { value: "option1", label: "Option 1" },
     { value: "option2", label: "Option 2" },
     { value: "option3", label: "Option 3" },
   ],
