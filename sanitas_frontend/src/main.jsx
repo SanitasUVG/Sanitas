15bc8811f00afc9eead5cbcfe75ef07ed993d27d
ElrohirGT <45268815+ElrohirGT@users.noreply.github.com>
2024-06-13 09:50:54 -0600

    feat: Added default dashboard sidebar component and refactores some code (#117)

--- sanitas_frontend/src/main.jsx
+++ sanitas_frontend/src/main.jsx
@@ -4,7 +4,9 @@ import "./index.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { ROUTES } from "./router.jsx";
 
-const router = createBrowserRouter(ROUTES);
+const router = createBrowserRouter(ROUTES, {
+  basename: import.meta.env.BASE_URL,
+});
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
