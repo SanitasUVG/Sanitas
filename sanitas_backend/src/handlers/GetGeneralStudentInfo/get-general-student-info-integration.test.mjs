26dcb424d67b922be9d6ead16b2d74a86aa97af3
DanielDubon <dub22233@uvg.edu.gt>
2024-06-13 13:59:25 -0600

    feat: get student info endpoint

---
+++ sanitas_backend/src/handlers/GetGeneralStudentInfo/get-general-student-info-integration.test.mjs
@@ -0,0 +1,39 @@
+import { beforeAll, describe, expect, test } from "@jest/globals";
+import axios from "axios";
+import { LOCAL_API_URL } from "../testHelpers.mjs";
+
+const API_URL = `${LOCAL_API_URL}/patient/student/`;
+
+describe("Student Handler", () => {
+  let studentId = 1;
+  let fakestudentId = 9999;
+
+  it("should return 403 if no CARNET is provided", async () => {
+    try {
+      await axios.get(API_URL);
+    } catch (error) {
+      expect(error.response.status).toBe(403);
+    }
+  });
+
+  it("should return a patient", async () => {
+    const response = await axios.get(API_URL + studentId);
+
+    expect(response).toBeDefined();
+    expect(response.status).toBe(200);
+
+    const user = response.data;
+    expect(user).toBeDefined();
+    expect(user.carnet).toBe("A01234567");
+    expect(user.career).toBe("Ingeniería en CC y TI");
+    expect(user.patientId).toBe(1);
+  });
+
+  it("should not found a patiend", async () => {
+    try {
+      const response = await axios.get(API_URL + fakestudentId);
+    } catch (error) {
+      expect(error.response.status).toBe(404);
+    }
+  });
+});
