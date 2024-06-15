eabfa480f7432fe9f25e76c6660380c9c5846b64
lemoonchild <ncastrom21@gmail.com>
2024-06-14 19:44:13 -0600

    feat: added style props to radio input

--- sanitas_frontend/src/components/Input/Radio/radio.jsx
+++ sanitas_frontend/src/components/Input/Radio/radio.jsx
@@ -10,18 +10,20 @@ import React from "react";
  * @param {string} props.label - The label text displayed next to the radio button.
  * @returns {React.Element} A labeled radio button element.
  */
-export default function RadioInput({ name, checked, onChange, label }) {
-  const styles = {
+export default function RadioInput({ name, checked, onChange, label, style = {} }) {
+  const defaultStyles = {
     label: {
       position: "relative",
       paddingLeft: "25px",
       cursor: "pointer",
       display: "inline-block",
+      ...style.label,
     },
     input: {
       position: "absolute",
       opacity: 0,
       cursor: "pointer",
+      ...style.input,
     },
     outerSpan: {
       position: "absolute",
@@ -34,6 +36,7 @@ export default function RadioInput({ name, checked, onChange, label }) {
       border: "1px solid #5B6670",
       display: "inline-block",
       padding: "3px",
+      ...style.outerSpan,
     },
     innerSpan: {
       display: checked ? "block" : "none",
@@ -41,14 +44,21 @@ export default function RadioInput({ name, checked, onChange, label }) {
       height: "100%",
       backgroundColor: "#0F6838",
       borderRadius: "50%",
+      ...style.innerSpan,
     },
   };
 
   return (
-    <label style={styles.label}>
-      <input type="radio" name={name} checked={checked} onChange={onChange} style={styles.input} />
-      <span style={styles.outerSpan}>
-        <span style={styles.innerSpan}></span>
+    <label style={defaultStyles.label}>
+      <input
+        type="radio"
+        name={name}
+        checked={checked}
+        onChange={onChange}
+        style={defaultStyles.input}
+      />
+      <span style={defaultStyles.outerSpan}>
+        <span style={defaultStyles.innerSpan}></span>
       </span>
       {label}
     </label>
