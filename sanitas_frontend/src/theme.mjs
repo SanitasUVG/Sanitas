65e112c7d73441ac212929444a573e5777f559a4
lemoonchild <ncastrom21@gmail.com>
2024-06-14 18:51:23 -0600

    feat: added a theme file to use in pages

---
+++ sanitas_frontend/src/theme.mjs
@@ -0,0 +1,20 @@
+export const colors = {
+  primaryBackground: "#0F6838", // Fondo principal
+  secondaryBackground: "#FFFFFF", // Fondo secundario
+  primaryText: "#1B1B1B", // Color principal de texto
+  titleText: "#3E8B43", // Color de títulos
+  statusApproved: "#9BBE43", // Color para estados aprobados
+  statusDenied: "#F05050", // Color para estados denegados
+  sidebarHover: "#E6E7E7", // Color de fondo para el hover en la barra lateral
+};
+
+export const fonts = {
+  titleFont: "Montserrat, sans-serif", // Fuente para títulos
+  textFont: "Lora, serif", // Fuente para texto o subtitulos (no usar bold)
+};
+
+export const fontSize = {
+  titleSize: "2rem", // Tamaño de fuente para títulos
+  subtitleSize: "1.25rem", // Tamaño de fuente para subtítulos
+  textSize: "1.06rem", // Tamaño de fuente para texto
+};
