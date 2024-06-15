7f066f962f8c721d9091a3d8fbe5e5d23920dbf7
lemoonchild <ncastrom21@gmail.com>
2024-06-15 14:52:33 -0600

    fix: fixed storybook

--- sanitas_frontend/src/views/AddPatientView/index.stories.jsx
+++ sanitas_frontend/src/views/AddPatientView/index.stories.jsx
@@ -1,5 +1,4 @@
 import { action } from "@storybook/addon-actions";
-import React from "react";
 import { MemoryRouter } from "react-router-dom";
 import { createEmptyStore } from "src/store.mjs";
 import { AddPatientView } from ".";
@@ -9,30 +8,24 @@ export default {
   component: AddPatientView,
   decorators: [
     (Story) => (
-      <MemoryRouter
-        initialEntries={[{ pathname: "/add-patient", state: { cui: "1234567890123" } }]}
-      >
+      <MemoryRouter>
         <Story />
       </MemoryRouter>
     ),
   ],
 };
 
+const defaultUseStore = createEmptyStore();
 
-const mockUseStore = () => ({
-  setSelectedPatientId: action("Set Selected Patient ID"),
-});
-
-const Template = (args) => <AddPatientView {...args} useStore={mockUseStore} />;
-
-export const Default = Template.bind({});
-Default.args = {
-  submitPatientData: async (patientData) => {
-    action("Submitting patient data")(patientData);
-    return Promise.resolve({
-      message: "Patient data submitted successfully",
-      patientId: "new_patient_id_123",
-    });
+export const Default = {
+  args: {
+    submitPatientData: async (patientData) => {
+      action("Submitting patient data")(patientData);
+      return Promise.resolve({
+        message: "Patient data submitted successfully",
+        patientId: "new_patient_id_123",
+      });
+    },
+    useStore: defaultUseStore,
   },
-  useStore: useStore,
 };
