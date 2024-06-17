43a773ffa35d40b16360c49199f2e15bbb4749a8
XavierLopez25 <lop22716@uvg.edu.gt>
2024-06-17 11:53:12 -0600

    feat: added function to calculate age

--- sanitas_frontend/src/utils/date.js
+++ sanitas_frontend/src/utils/date.js
@@ -5,3 +5,16 @@ export const formatDate = (dateString) => {
   const day = String(date.getUTCDate()).padStart(2, "0");
   return `${year}-${month}-${day}`;
 };
+
+export const calculateYearsBetween = (dateString) => {
+  const formattedDate = formatDate(dateString);
+  const startDate = new Date(formattedDate);
+  const today = new Date();
+  const differenceInYears = today.getFullYear() - startDate.getFullYear();
+  const monthDifference = today.getMonth() - startDate.getMonth();
+
+  if (monthDifference < 0 || (monthDifference === 0 && today.getDate() < startDate.getDate())) {
+    return differenceInYears - 1;
+  }
+  return differenceInYears;
+};
