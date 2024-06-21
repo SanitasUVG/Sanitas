b721c3496265917dd29636a04521350a2158c6cc
lemoonchild <ncastrom21@gmail.com>
2024-06-21 13:07:44 -0600

    feat: added JSDoc

--- sanitas_frontend/src/components/AppointmentSurgeryCard/index.jsx
+++ sanitas_frontend/src/components/AppointmentSurgeryCard/index.jsx
@@ -1,6 +1,26 @@
 import React, { useState } from "react";
 import { colors, fonts } from "src/theme.mjs";
 
+/**
+ * @typedef {Object} AppointmentSurgeryCardProps
+ * @property {'surgical' | 'appointment'} type - Distinguishes between surgical and appointment types to render appropriate information.
+ * @property {string} year - The year of the surgery or appointment.
+ * @property {string} surgeryType - The type of surgery, only applicable if `type` is 'surgical'.
+ * @property {string} date - The date of the appointment, only applicable if `type` is 'appointment'.
+ * @property {string} reason - The reason for the consultation, applicable only if `type` is 'appointment'.
+ * @property {React.MouseEventHandler<HTMLDivElement>} onClick - The callback function to be executed when the card is clicked.
+ *
+ * Renders a card component that displays either surgical or appointment details based on the `type` prop.
+ * The card is interactive and changes its style on hover. It supports click interactions that trigger the provided `onClick` callback.
+ *
+ * The surgical card displays the year and type of surgery with optional truncation of long text.
+ * The appointment card shows the date and reason for the consultation, also with optional truncation.
+ *
+ * Styles are applied directly through inline styles, and hover effects are managed with internal state.
+ *
+ * @param {AppointmentSurgeryCardProps} props - The properties passed to the card component.
+ * @returns {JSX.Element} The React component rendering the card with either surgical or appointment information.
+ */
 export default function AppointmentSurgeryCard({ type, year, surgeryType, date, reason, onClick }) {
   const [hover, setHover] = useState(false);
 
