3110c9188286105c672f25584c623851440e5027
Nahomy Castro <98111102+lemoonchild@users.noreply.github.com>
2024-07-17 19:05:41 -0600

    feat: gui and css to the surgical history view (#129)
    * feat: added surgical history path
    
    * feat: added api response for surgical history
    
    * fix: fixed padding top for the name
    
    * feat: added surgical history
    
    * feat: added surgical and appointment card
    
    * feat: added cursor pointer and change colors based on theme
    
    * fix: change the border width for the card
    
    * fix: fixed the get and update function
    
    * fix: change route erros
    
    * feat: added new features to surgical history view
    
    * fix: import updatesurgicalhistory method
    
    * feat: added JSDoc
    
    * feat: added padding to return button
    
    * feat: added read only prop
    
    * feat: added read only to base input and drop down
    
    * fix: fixed update information error in surgery history
    
    * fix: modify truncate function
    
    * fix: fixed an error in add new surgical history function
    
    * fix: fixed a React error in Input
    
    * feat: added storybook and vitest
    
    * fix: change story component name
    
    * fix: fixed the returns
    
    * feat: added an index path
    
    * fix: added an error state
    
    * feat: added surgical history to sidebar
    
    * fix: fixed api functions
    
    * feat: added toast
    
    * feat: added toast to the proyect
    
    * fix: update InformationCard
    
    * feat: added Suspense to the view
    
    * fix: eliminate some duplicated files
    
    * fix: fixed vitest
    
    * fix: fix JSDoc and space in button
    
    * fix: fix a bug
    
    * fix: changed the format of get and update surgical history endpoint
    
    * fix: fixed api call
    
    * fix: fixed storybook
    
    * fix: fixed not displaying errors
    
    * fix: fixed error in test
    
    * fix: fixed order by year
    
    ---------
    
    Co-authored-by: ElrohirGT <elrohirgt@gmail.com>

--- sanitas_backend/src/handlers/GetSurgicalHistory/get-surgical-history-patient.integration.test.mjs
+++ sanitas_backend/src/handlers/GetSurgicalHistory/get-surgical-history-patient.integration.test.mjs
@@ -3,29 +3,32 @@ import axios from "axios";
 import { createTestPatient, LOCAL_API_URL, updatePatientSurgicalHistory } from "../testHelpers.mjs";
 
 const API_URL = `${LOCAL_API_URL}patient/surgical-history/`;
-let patientId;
 
-async function createPatient(hasHistory) {
+async function createPatient() {
   // Create a test patient first
-  const patientResponse = await createTestPatient();
-  patientId = patientResponse;
+  const patientId = await createTestPatient();
 
   // Now update their surgical history
   await updatePatientSurgicalHistory(patientId, {
-    hasSurgicalEvent: hasHistory,
-    surgicalEventData: [
-      {
-        surgeryType: "Appendectomy",
-        surgeryYear: "2023",
-        complications: "None",
+    medicalHistory: {
+      surgeries: {
+        version: 1,
+        data: [
+          {
+            surgeryType: "Appendectomy",
+            surgeryYear: "2023",
+            complications: "None",
+          },
+        ],
       },
-    ],
+    },
   });
 
   return patientId;
 }
 
 describe("Get Surgical History integration tests", () => {
+  let patientId;
   beforeAll(async () => {
     patientId = await createPatient(true);
   });
@@ -36,43 +39,40 @@ describe("Get Surgical History integration tests", () => {
     expect(response).toBeDefined();
     expect(response.status).toBe(200);
 
+    /** @type {import("utils/defaultValues.mjs").APISurgicalHistory} */
     const surgicalHistory = response.data;
 
     expect(surgicalHistory).toBeDefined();
     expect(surgicalHistory.patientId).toBe(patientId);
-    expect(surgicalHistory.hasSurgicalEvent).toBeTruthy();
-    expect(surgicalHistory.surgicalEventData.length).toBeGreaterThan(0);
-    expect(surgicalHistory.surgicalEventData[0].surgeryType).toBe("Appendectomy");
+    expect(surgicalHistory.medicalHistory.surgeries.data.length).toBe(1);
+    expect(surgicalHistory.medicalHistory.surgeries.data[0].surgeryType).toBe("Appendectomy");
   });
 
-  test("Fail to retrieve surgical history for non-existent patient", async () => {
+  test("Retrieve default surgical history for non-existent patient", async () => {
     const nonExistentPatientId = 999999; // Assuming this ID does not exist
     const response = await axios.get(`${API_URL}${nonExistentPatientId}`, {
       validateStatus: () => true,
     });
 
     expect(response).toBeDefined();
-    expect(response.status).toBe(404);
+    expect(response.status).toBe(200);
 
-    const { error } = response.data;
-    expect(error).toBe("No surgical history found for the provided ID.");
+    /** @type {import("utils/defaultValues.mjs").APISurgicalHistory} */
+    const surgicalHistory = response.data;
+
+    expect(surgicalHistory).toBeDefined();
+    expect(surgicalHistory.patientId).toBe(nonExistentPatientId);
+    expect(surgicalHistory.medicalHistory.surgeries.data.length).toBe(0);
   });
 
   test("Invalid ID provided", async () => {
-    const invalidId = "100000000"; // Assuming this is an invalid ID format
+    const invalidId = "abckdj";
     const response = await axios.get(`${API_URL}${invalidId}`, { validateStatus: () => true });
 
     expect(response).toBeDefined();
-    expect(response.status).toBe(404);
+    expect(response.status).toBe(400);
 
     const { error } = response.data;
-    expect(error).toBe("No surgical history found for the provided ID.");
+    expect(error).toBe("Invalid request: No valid patientId supplied!");
   });
-
-  test("Patient with hasSurgicalEvent false is not returning info.", async () => {
-    patientId = await createPatient(false);
-    const response = await axios.get(`${API_URL}${patientId}`, { validateStatus: () => true });
-    expect(response).toBeDefined();
-    expect(response.data.surgicalEventData).toEqual([]);
-  }, 10000);
 });
--- sanitas_frontend/src/router.jsx
+++ sanitas_frontend/src/router.jsx
@@ -1,17 +1,20 @@
-import { useNavigate } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 import {
   checkCui,
   getCollaboratorInformation,
   getGeneralPatientInformation,
   getStudentPatientInformation,
+  getSurgicalHistory,
   searchPatient,
   submitPatientData,
   updateCollaboratorInformation,
   updateGeneralPatientInformation,
   updateStudentPatientInformation,
+  updateSurgicalHistory,
 } from "./dataLayer.mjs";
 import { createEmptyStore } from "./store.mjs";
 import { AddPatientView } from "./views/AddPatientView";
+import { SurgicalHistory } from "./views/History/Surgical";
 import SearchPatientView from "./views/SearchPatientView";
 import UpdateInfoView from "./views/UpdateGeneralInformationView";
 
@@ -25,6 +28,7 @@ export const NAV_PATHS = {
 
 export const UPDATE_PATIENT_NAV_PATHS = {
   GENERAL_INFORMATION: "general",
+  SURGICAL_HISTORY: "surgical",
   // TODO: Add other Navigation routes...
 };
 
@@ -38,7 +42,10 @@ export const DEFAULT_DASHBOARD_SIDEBAR_PROPS = {
     navigate(NAV_PATHS.SEARCH_PATIENT);
   },
   navigateToGeneral: (navigate) => {
-    navigate(UPDATE_PATIENT_NAV_PATHS.GENERAL_INFORMATION);
+    navigate(`${NAV_PATHS.UPDATE_PATIENT}/${UPDATE_PATIENT_NAV_PATHS.GENERAL_INFORMATION}`);
+  },
+  navigateToSurgical: (navigate) => {
+    navigate(`${NAV_PATHS.UPDATE_PATIENT}/${UPDATE_PATIENT_NAV_PATHS.SURGICAL_HISTORY}`);
   },
   // TODO: Add other Navigation routes...
 };
@@ -51,6 +58,16 @@ const updateInfoView = (
     updateStudentPatientInformation={updateStudentPatientInformation}
     getCollaboratorInformation={getCollaboratorInformation}
     updateCollaboratorInformation={updateCollaboratorInformation}
+    sidebarConfig={DEFAULT_DASHBOARD_SIDEBAR_PROPS}
+    useStore={useStore}
+  />
+);
+
+const surgicalHistoryView = (
+  <SurgicalHistory
+    getBirthdayPatientInfo={getGeneralPatientInformation}
+    getSurgicalHistory={getSurgicalHistory}
+    updateSurgicalHistory={updateSurgicalHistory}
     sidebarConfig={DEFAULT_DASHBOARD_SIDEBAR_PROPS}
     useStore={useStore}
   />
@@ -73,13 +90,21 @@ export const ROUTES = [
   },
   {
     path: NAV_PATHS.UPDATE_PATIENT,
-    // The default page is the update general information view.
-    element: updateInfoView,
+    element: <Outlet />,
     children: [
+      {
+        index: true,
+        element: updateInfoView,
+      },
       {
         path: UPDATE_PATIENT_NAV_PATHS.GENERAL_INFORMATION,
         element: updateInfoView,
       },
+      {
+        path: UPDATE_PATIENT_NAV_PATHS.SURGICAL_HISTORY,
+        element: surgicalHistoryView,
+      },
+      // TODO: Add more routes...
     ],
   },
 ];
