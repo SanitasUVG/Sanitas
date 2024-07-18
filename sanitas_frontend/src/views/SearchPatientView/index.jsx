55a0cf3aa9caacd10f1b4616d2a06ea4d9273737
XavierLopez25 <lop22716@uvg.edu.gt>
2024-07-18 17:06:26 -0600

    fix: removed unused imports

--- sanitas_frontend/src/views/SearchPatientView/index.jsx
+++ sanitas_frontend/src/views/SearchPatientView/index.jsx
@@ -1,6 +1,6 @@
 import logoutIcon from "@tabler/icons/outline/door-exit.svg";
 import settingsIcon from "@tabler/icons/outline/settings.svg";
-import React, { Suspense, useEffect, useState } from "react";
+import React, { Suspense, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import BorderDecoLower from "src/assets/images/BorderDecoLower.png";
 import BorderDecoUpper from "src/assets/images/BorderDecoUpper.png";
@@ -11,75 +11,10 @@ import DropdownMenu from "src/components/DropdownMenu";
 import { SearchInput } from "src/components/Input";
 import PatientCard from "src/components/PatientCard";
 import { NAV_PATHS } from "src/router";
-import { colors, fonts, fontSize } from "src/theme.mjs";
-import { calculateYearsBetween, formatDate } from "src/utils/date";
-import { delay } from "src/utils/general";
+import { colors, fonts } from "src/theme.mjs";
+import { adjustHeight, adjustWidth } from "src/utils/measureScaling";
 import WrapPromise from "src/utils/promiseWrapper";
 import useWindowSize from "src/utils/useWindowSize";
-
-/**
- * Adjusts the REM values based on the width of the screen.
- * The function scales the input REM values by a scaleFactor determined by the screen width.
- *
- * @param {number} width - The current width of the screen in pixels.
- * @param {string} remValues - A string containing space-separated REM values to be adjusted.
- * @returns {string} A string of space-separated REM values adjusted based on the screen width.
- * @example
- * // returns "9.300rem 18.600rem"
- * adjustWidth(1400, "10rem 20rem");
- */
-const adjustWidth = (width, remValues) => {
-  let scaleFactor;
-  if (width >= 1538) {
-    scaleFactor = 1.0;
-  } else if (width >= 1440) {
-    scaleFactor = 0.93;
-  } else if (width >= 1280) {
-    scaleFactor = 0.83;
-  } else {
-    scaleFactor = 1.0;
-  }
-
-  return remValues
-    .split(" ")
-    .map((rem) => {
-      const value = Number.parseFloat(rem) * scaleFactor;
-      return `${value.toFixed(3)}rem`;
-    })
-    .join(" ");
-};
-
-/**
- * Adjusts the REM values based on the height of the screen.
- * The function scales the input REM values by a scaleFactor determined by the screen height.
- *
- * @param {number} height - The current height of the screen in pixels.
- * @param {string} remValues - A string containing space-separated REM values to be adjusted.
- * @returns {string} A string of space-separated REM values adjusted based on the screen height.
- * @example
- * // returns "9.400rem 18.800rem"
- * adjustHeight(850, "10rem 20rem");
- */
-const adjustHeight = (height, remValues) => {
-  let scaleFactor;
-  if (height >= 950) {
-    scaleFactor = 1.0;
-  } else if (height >= 900) {
-    scaleFactor = 0.94;
-  } else if (height >= 800) {
-    scaleFactor = 0.84;
-  } else {
-    scaleFactor = 1.0;
-  }
-
-  return remValues
-    .split(" ")
-    .map((rem) => {
-      const value = Number.parseFloat(rem) * scaleFactor;
-      return `${value.toFixed(3)}rem`;
-    })
-    .join(" ");
-};
 
 /**
  * @typedef {Object} PatientPreview
