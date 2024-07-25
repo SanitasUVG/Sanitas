0a048200005a7cdc6f6da068bf46d484dcdad35b
DanielDubon <100601875+DanielDubon@users.noreply.github.com>
2024-07-25 12:36:40 -0600

    feat: loading state on addpatient added (#140)

--- sanitas_frontend/src/views/AddPatientView/index.test.jsx
+++ sanitas_frontend/src/views/AddPatientView/index.test.jsx
@@ -1,6 +1,7 @@
 import { fireEvent, render, screen, waitFor } from "@testing-library/react";
 import { MemoryRouter } from "react-router-dom";
 import { createEmptyStore } from "src/store.mjs";
+import * as store from "src/store.mjs";
 import { describe, expect, test, vi } from "vitest";
 import { AddPatientView } from "../AddPatientView";
 
@@ -20,8 +21,42 @@ describe("AddPatientView tests", () => {
     const button = screen.getByText("Registrar información");
     fireEvent.click(button);
 
-    expect(window.alert).toHaveBeenCalledWith("El CUI debe contener exactamente 13 caracteres.");
+    const errorMessages = screen.getAllByText("El CUI debe contener exactamente 13 caracteres.");
+    expect(errorMessages.length).toBeGreaterThan(0);
     expect(submitPatientData).not.toHaveBeenCalled();
+  });
+
+  test("Displays error message when submitPatientData fails", async () => {
+    const errorMessage = "Error conexion";
+    const useStore = createEmptyStore();
+
+    const submitPatientData = vi.fn().mockResolvedValue({ error: errorMessage });
+    window.alert = vi.fn();
+    render(
+      <MemoryRouter>
+        <AddPatientView submitPatientData={submitPatientData} useStore={useStore} />
+      </MemoryRouter>,
+    );
+
+    const input = screen.getByPlaceholderText("CUI");
+    fireEvent.change(input, { target: { value: "1234567890123" } });
+    const namesInput = screen.getByPlaceholderText("Nombres");
+    fireEvent.change(namesInput, { target: { value: "Test" } });
+    const surnamesInput = screen.getByPlaceholderText("Apellidos");
+    fireEvent.change(surnamesInput, { target: { value: "User" } });
+    const genderInput = screen.getByLabelText("Femenino");
+    fireEvent.click(genderInput);
+    const birthDateInput = screen.getByPlaceholderText("Fecha de nacimiento");
+    fireEvent.change(birthDateInput, { target: { value: "2000-01-01" } });
+    const button = screen.getByText("Registrar información");
+    fireEvent.click(button);
+
+    await waitFor(() => {
+      const errorMessages = screen.getAllByText(
+        `Lo sentimos! Ha ocurrido un error al enviar los datos! ${errorMessage}`,
+      );
+      expect(errorMessages.length).toBeGreaterThan(0);
+    });
   });
 
   test("Valid CUI triggers submitPatientData function", async () => {
@@ -58,36 +93,6 @@ describe("AddPatientView tests", () => {
         }),
       )
     );
-  });
-
-  test("Displays error message when submitPatientData fails", async () => {
-    const errorMessage = "Error de conexión";
-    const useStore = createEmptyStore();
-
-    const submitPatientData = vi.fn().mockRejectedValue(new Error(errorMessage));
-    window.alert = vi.fn();
-    render(
-      <MemoryRouter>
-        <AddPatientView submitPatientData={submitPatientData} useStore={useStore} />
-      </MemoryRouter>,
-    );
-
-    const input = screen.getByPlaceholderText("CUI");
-    fireEvent.change(input, { target: { value: "1234567890123" } });
-    const namesInput = screen.getByPlaceholderText("Nombres");
-    fireEvent.change(namesInput, { target: { value: "Test" } });
-    const surnamesInput = screen.getByPlaceholderText("Apellidos");
-    fireEvent.change(surnamesInput, { target: { value: "User" } });
-    const genderInput = screen.getByLabelText("Femenino");
-    fireEvent.click(genderInput);
-    const birthDateInput = screen.getByPlaceholderText("Fecha de nacimiento");
-    fireEvent.change(birthDateInput, { target: { value: "2000-01-01" } });
-    const button = screen.getByText("Registrar información");
-    fireEvent.click(button);
-
-    await waitFor(() => {
-      expect(window.alert).toHaveBeenCalledWith(`Error al enviar datos: ${errorMessage}`);
-    });
   });
 
   test("CUI input only allows numeric characters and limits to 13 digits", () => {
