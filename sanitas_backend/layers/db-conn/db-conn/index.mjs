22f635f0648f65ebec2183539da1169daa742a58
ElrohirGT <elrohirgt@gmail.com>
2024-08-11 09:08:27 -0600

    feat: Added DB is doctor method

--- sanitas_backend/layers/db-conn/db-conn/index.mjs
+++ sanitas_backend/layers/db-conn/db-conn/index.mjs
@@ -11,3 +11,20 @@ export function getPgClient(connectionString) {
 		connectionString,
 	});
 }
+
+/**
+	* Given an email and a DB connection, checks if the user is a doctor or not.
+	* @param {pg.Client} client - The PG Client
+	* @param {string} email - The user email
+	* @returns {boolean|{error: *}} If successful, will return true if the user is a doctor, false otherwise. If not successful an object with an error will be returned.
+	*/
+export async function isDoctor(client, email) {
+	const query = "SELECT EXISTS(SELECT 1 FROM DOCTOR WHERE email=$1) AS 'valid'";
+	const params = [email];
+	try {
+		let result = await client.query(query, params);
+		return result.rows[0].valid;
+	} catch (error) {
+		return {error}
+	}
+}
