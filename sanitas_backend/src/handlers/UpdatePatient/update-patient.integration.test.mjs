bacf9b6d50a131660548cede9cb239b07e1f4e91
ElrohirGT <elrohirgt@gmail.com>
2024-08-23 11:29:24 -0600

    fix: tests

--- sanitas_backend/src/handlers/UpdatePatient/update-patient.integration.test.mjs
+++ sanitas_backend/src/handlers/UpdatePatient/update-patient.integration.test.mjs
@@ -71,7 +71,9 @@ describe("Update Patient integration tests", () => {
 
 		// Verificar que el error sea el esperado
 		expect(response.status).toBe(400);
-		expect(response.data.error).toBe("No patient with the given ID found!");
+		expect(response.data.error).toBe(
+			"No se encontraron registros con el ID proporcionado.",
+		);
 	});
 
 	test("a patient can't call the endpoint", async () => {
