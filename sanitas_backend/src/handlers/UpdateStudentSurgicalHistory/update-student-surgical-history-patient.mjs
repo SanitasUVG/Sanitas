60e664ba854d956ade6c54039111ba706aded6ba
Angel Ortega <martinortegayung@gmail.com>
2024-08-29 13:23:57 -0600

    feat: student surgery history (#197)
    * feat: scrum 231 student login and surgery history
    
    * fix: fixed the linting
    
    * fix: changed format again
    
    * feat: added endpoints for student surgical history
    
    * fix: extracted checking function
    
    * fix: fixed lint warnings
    
    * fix: formatting
    
    * fix: fixed tests
    
    * fix: cleaned some errors in integration tests
    
    * fix: eliminated horizontal scroll bar and did some responsive changes
    
    * fix: format
    
    * fix: fixed crash on login
    
    ---------
    
    Co-authored-by: ElrohirGT <elrohirgt@gmail.com>

---
+++ sanitas_backend/src/handlers/UpdateStudentSurgicalHistory/update-student-surgical-history-patient.mjs
@@ -0,0 +1,178 @@
+import { getPgClient, isDoctor } from "db-conn";
+import { logger, withRequest } from "logging";
+import {
+	createResponse,
+	decodeJWT,
+	mapToAPISurgicalHistory,
+	requestDataEditsDBData,
+} from "utils/index.mjs";
+
+/**
+ * Handles the HTTP POST request to update or create surgical history for a specific patient.
+ * @param {import('aws-lambda').APIGatewayProxyEvent} event
+ * @param {import('aws-lambda').APIGatewayProxyResult} context
+ * @returns {Promise<import('aws-lambda').APIGatewayProxyResult>} The API response object with status code and body.
+ */
+export const updateStudentSurgicalHistoryHandler = async (event, context) => {
+	withRequest(event, context);
+	const responseBuilder = createResponse().addCORSHeaders("POST");
+
+	if (event.httpMethod !== "POST") {
+		return responseBuilder
+			.setStatusCode(405)
+			.setBody({ error: "Method Not Allowed" })
+			.build();
+	}
+
+	logger.info({ headers: event.headers }, "Received headers...");
+	const jwt = event.headers.Authorization;
+
+	logger.info({ jwt }, "Parsing JWT...");
+	const tokenInfo = decodeJWT(jwt);
+	if (tokenInfo.error) {
+		logger.error({ error: tokenInfo.error }, "JWT couldn't be parsed!");
+		return responseBuilder
+			.setStatusCode(400)
+			.setBody({ error: "JWT couldn't be parsed" })
+			.build();
+	}
+	const { email } = tokenInfo;
+	logger.info({ tokenInfo }, "JWT Parsed!");
+
+	let client;
+
+	try {
+		const url = process.env.POSTGRES_URL;
+		logger.info({ url }, "Connecting to DB...");
+		client = getPgClient(url);
+		await client.connect();
+		logger.info("Connected!");
+
+		const itsDoctor = await isDoctor(client, email);
+		if (itsDoctor.error) {
+			const msg = "An error occurred while trying to check if user is doctor!";
+			logger.error({ error: itsDoctor.error }, msg);
+			return responseBuilder.setStatusCode(500).setBody({ error: msg }).build();
+		}
+
+		if (itsDoctor) {
+			const msg = "Unauthorized, you're a doctor!";
+			const body = { error: msg };
+			logger.error(body, msg);
+			return responseBuilder.setStatusCode(401).setBody(body).build();
+		}
+		logger.info(`${email} is not a doctor!`);
+
+		logger.info({ eventBody: event.body }, "Parsing event body...");
+		/** @type {import("utils/defaultValues.mjs").APISurgicalHistory}  */
+		const { patientId: id, medicalHistory } = JSON.parse(event.body);
+		logger.info("Event body parsed!");
+
+		if (!id) {
+			logger.error("No id provided!");
+			return responseBuilder
+				.setStatusCode(400)
+				.setBody({ error: "Invalid input: Missing or empty required fields." })
+				.build();
+		}
+
+		await client.query("begin");
+
+		const studentSearchValues = [id];
+
+		const getPatientQuery = `
+			SELECT * FROM antecedentes_quirurgicos WHERE id_paciente = $1;
+		`;
+
+		logger.info(
+			{ getPatientQuery, studentSearchValues },
+			"Searching patient in DB...",
+		);
+		const patientResult = await client.query(
+			getPatientQuery,
+			studentSearchValues,
+		);
+		logger.info("Done searching!");
+
+		if (patientResult.rowCount > 0) {
+			const oldData =
+				patientResult.rows[0].antecedente_quirurgico_data.surgeries.data;
+			const newData = medicalHistory.surgeries.data;
+
+			logger.info({ oldData }, "Data of the patient in DB currently...");
+			logger.info({ medicalHistory }, "Data coming in...");
+
+			const repeatingData = requestDataEditsDBData(newData, oldData);
+
+			if (repeatingData) {
+				logger.error("Student trying to update info already saved!");
+				return responseBuilder
+					.setStatusCode(400)
+					.setBody({
+						error: "Invalid input: Students cannot update saved info.",
+					})
+					.build();
+			}
+		}
+
+		const values = [id, true, medicalHistory];
+
+		const upsertQuery = `
+				INSERT INTO antecedentes_quirurgicos (id_paciente, antecedente_quirurgico, antecedente_quirurgico_data)
+				VALUES ($1, $2, $3)
+				ON CONFLICT (id_paciente) DO UPDATE
+				SET antecedente_quirurgico = COALESCE(EXCLUDED.antecedente_quirurgico, antecedentes_quirurgicos.antecedente_quirurgico),
+						antecedente_quirurgico_data = COALESCE(EXCLUDED.antecedente_quirurgico_data, antecedentes_quirurgicos.antecedente_quirurgico_data)
+				RETURNING *;
+		`;
+
+		logger.info({ upsertQuery, values }, "Inserting/Updating values in DB...");
+		const result = await client.query(upsertQuery, values);
+		logger.info("Done inserting/updating!");
+
+		await client.query("commit");
+
+		if (result.rowCount === 0) {
+			logger.error("No value was inserted/updated in the DB!");
+
+			return responseBuilder
+				.setStatusCode(404)
+				.setBody({ message: "Failed to insert or update surgical history." })
+				.build();
+		}
+
+		logger.info("Mapping DB response into API response...");
+		const updatedRecord = result.rows[0];
+		const formattedResponse = mapToAPISurgicalHistory(updatedRecord);
+		logger.info({ formattedResponse }, "Done! Responding with:");
+		return responseBuilder
+			.setStatusCode(200)
+			.setBody(formattedResponse)
+			.build();
+	} catch (error) {
+		await client.query("rollback");
+		logger.error(
+			{ error },
+			"An error occurred while updating surgical history!",
+		);
+
+		if (error.code === "23503") {
+			return responseBuilder
+				.setStatusCode(404)
+				.setBody({ error: "Patient not found with the provided ID." })
+				.build();
+		}
+
+		return responseBuilder
+			.setStatusCode(500)
+			.setBody({
+				error: "Failed to update surgical history due to an internal error.",
+				details: error.message,
+			})
+			.build();
+	} finally {
+		if (client) {
+			await client.end();
+		}
+	}
+};
---
+++ sanitas_frontend/src/views/StudentWelcomeView/index.jsx
@@ -0,0 +1,244 @@
+import { useNavigate } from "react-router-dom";
+import SanitasLogo from "src/assets/images/logoSanitas.png";
+import uvgLogo from "src/assets/images/uvgLogo.jpg";
+import clinica from "src/assets/images/clinica.png";
+import BaseButton from "src/components/Button/Base/index";
+import { fonts, fontSize } from "src/theme.mjs";
+import { NAV_PATHS } from "src/router";
+
+export default function StudentWelcomeView() {
+	const navigate = useNavigate();
+
+	return (
+		<div
+			style={{
+				backgroundColor: "#0F6838",
+				height: "100vh",
+				width: "100vw",
+				padding: "2rem",
+				overflowX: "hidden",
+			}}
+		>
+			<div
+				style={{
+					display: "flex",
+					justifyContent: "space-between",
+					backgroundColor: "#FFFFFF",
+					width: "100%",
+					height: "122px",
+					borderRadius: "8.27px",
+					marginBottom: "66px",
+				}}
+			>
+				<div
+					style={{
+						display: "flex",
+						flexDirection: "column",
+						justifyContent: "center",
+						height: "100%",
+					}}
+				>
+					<img
+						style={{
+							width: "102.99px",
+							height: "100px",
+							gap: "0px",
+							opacity: "0px",
+						}}
+						src={uvgLogo}
+						alt="uvgLogo"
+					/>
+				</div>
+				<div
+					style={{
+						display: "flex",
+						flexDirection: "column",
+						justifyContent: "center",
+						height: "100%",
+					}}
+				>
+					<p
+						style={{
+							width: "637.56px",
+							height: "53px",
+							top: "95px",
+							left: "354.23px",
+							gap: "0px",
+							opacity: "0px",
+							fontFamily: "Montserrat",
+							fontSize: "43.2px",
+							fontWeight: "700",
+							lineHeight: "52.66px",
+							textAlign: "center",
+							color: "rgba(62, 139, 67, 1)",
+							alignItems: "center",
+						}}
+					>
+						¡Bienvenid@ a Clínica UVG!
+					</p>
+				</div>
+				<div
+					style={{
+						display: "flex",
+						flexDirection: "column",
+						justifyContent: "center",
+						height: "100%",
+					}}
+				>
+					<img
+						style={{
+							width: "135.99px",
+							height: "67px",
+							gap: "0px",
+							opacity: "0px",
+						}}
+						src={SanitasLogo}
+						alt="SanitasLogo"
+					/>
+				</div>
+			</div>
+
+			<div
+				style={{
+					display: "flex",
+					justifyContent: "space-between",
+					minWidth: "805px",
+					height: "70%",
+					borderRadius: "8.27px",
+				}}
+			>
+				<div
+					style={{
+						display: "flex",
+						flexDirection: "column",
+						justifyContent: "center",
+						marginRight: "1em",
+						backgroundColor: "#FFFFFF",
+						padding: "77px",
+						borderRadius: "8.27px",
+						overflowY: "scroll",
+					}}
+				>
+					<p
+						style={{
+							fontFamily: "Lora",
+							fontSize: "20px",
+							fontWeight: "400",
+							lineHeight: "30px",
+							textAlign: "center",
+							marginBottom: "1.5em",
+						}}
+					>
+						El siguiente formulario tiene como objetivo recolectar datos médicos
+						esenciales para nuestra clínica médica. Con esta información,
+						buscamos ofrecer una atención más precisa y personalizada a cada
+						paciente que nos visite.
+					</p>
+					<p
+						style={{
+							fontFamily: "Lora",
+							fontSize: "20px",
+							fontWeight: "700",
+							lineHeight: "30px",
+							textAlign: "center",
+							marginBottom: "1.5em",
+						}}
+					>
+						Es importante destacar que los datos proporcionados en este
+						formulario son confidenciales y solo las doctoras de la clínica
+						tendrán acceso a ellos.
+					</p>
+					<p
+						style={{
+							fontFamily: "Lora",
+							fontSize: "20px",
+							fontWeight: "400",
+							lineHeight: "30px",
+							textAlign: "center",
+							marginBottom: "5em",
+						}}
+					>
+						Agradecemos su colaboración y confianza.
+					</p>
+					<BaseButton
+						text="Continuar"
+						style={{
+							alignSelf: "center",
+							fontFamily: fonts.titleFont,
+							fontSize: fontSize.textSize,
+							width: "388px",
+							minHeight: "43px",
+							padding: "0px 16px 0px 16px",
+							gap: "8px",
+							borderRadius: "8px",
+							opacity: "0px",
+						}}
+						onClick={() => navigate(NAV_PATHS.PATIENT_FORM, { replace: true })}
+					/>
+				</div>
+				<div
+					style={{
+						minWidth: "464px",
+						display: "flex",
+						flexDirection: "column",
+						justifyContent: "space-between",
+						gap: "1.5em",
+						borderRadius: "8.27px",
+						opacity: "0px",
+					}}
+				>
+					<img
+						style={{
+							width: "464px",
+							height: "200%",
+							top: "249px",
+							left: "851px",
+							gap: "0",
+							borderRadius: "8.27px",
+							opacity: "0px",
+						}}
+						src={clinica}
+						alt="clinica"
+					/>
+					<div
+						style={{
+							width: "464px",
+							height: "100%",
+							display: "flex",
+							flexDirection: "column",
+							justifyContent: "center",
+							marginRight: "1em",
+							backgroundColor: "#FFFFFF",
+							padding: "1em",
+							borderRadius: "8.27px",
+						}}
+					>
+						<p
+							style={{
+								fontFamily: "Lora",
+								fontSize: "14.7px",
+								fontWeight: "400",
+								lineHeight: "22.05px",
+								textAlign: "center",
+								marginBottom: "1.5em",
+							}}
+						>
+							Para conocer más información de la clínica médica presiona aquí:
+						</p>
+						<p
+							style={{
+								fontFamily: "Lora",
+								fontSize: "14.7px",
+								fontWeight: "700",
+								lineHeight: "22.05px",
+								textAlign: "center",
+							}}
+						>
+							https://noticias.uvg.edu.gt/en-uvg-contamos-con-estaciones-de-emergencia-para-atender-a-la-comunidad/
+						</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+}
