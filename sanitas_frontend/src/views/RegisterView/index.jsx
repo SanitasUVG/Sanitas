bf839bef4db8009f999a7bafb25e12f325723478
Xavier López (Nathan) <105880705+XavierLopez25@users.noreply.github.com>
2024-09-26 23:16:08 -0600

    feat: added email verification modal to Register View (#218)
    * feat: added email verification modal to Register View
    
    * fix: fixed responsiveness
    
    * fix: fixed logic

--- sanitas_frontend/src/views/RegisterView/index.jsx
+++ sanitas_frontend/src/views/RegisterView/index.jsx
@@ -9,6 +9,8 @@ import Throbber from "src/components/Throbber";
 import { NAV_PATHS } from "src/router";
 import { colors, fonts, fontSize } from "src/theme.mjs";
 import WrapPromise from "src/utils/promiseWrapper";
+import { adjustHeight, adjustWidth } from "src/utils/measureScaling";
+import useWindowSize from "src/utils/useWindowSize";
 
 /**
  * @typedef {Object} RegisterViewProps
@@ -24,6 +26,10 @@ export default function RegisterView({ registerUser }) {
 		width: "100%",
 		padding: ".8rem",
 	};
+
+	const { width, height } = useWindowSize();
+
+	const [showPopup, setShowPopup] = useState(false);
 
 	const Child = () => {
 		const navigate = useNavigate();
@@ -44,7 +50,10 @@ export default function RegisterView({ registerUser }) {
 		if (registerResource !== null) {
 			const response = registerResource.read();
 			if (!response.error) {
-				navigate(NAV_PATHS.LOGIN_USER, { replace: true });
+				setShowPopup(true); // Mostrar el popup
+				setTimeout(() => {
+					navigate(NAV_PATHS.LOGIN_USER, { replace: true });
+				}, 7000);
 			} else {
 				console.log(response);
 				const errorType = response.error.code;
@@ -271,7 +280,80 @@ export default function RegisterView({ registerUser }) {
 
 	return (
 		<Suspense fallback={<LoadingView />}>
-			<Child />
+			<div
+				style={{
+					backgroundImage: `url(${backgroundImage})`,
+					backgroundColor: "black",
+					backgroundSize: "cover",
+					width: "100vw",
+					height: "100vh",
+					display: "grid",
+					alignItems: "center",
+					justifyItems: "center",
+				}}
+			>
+				{showPopup && (
+					<div
+						style={{
+							position: "fixed",
+							top: 0,
+							left: 0,
+							width: "100%",
+							height: "100%",
+							backgroundColor: "rgba(0,0,0,0.5)",
+							display: "flex",
+							justifyContent: "center",
+							alignItems: "center",
+							zIndex: 1000,
+						}}
+					>
+						<div
+							style={{
+								backgroundColor: "white",
+								padding: adjustWidth(width, "1.25rem"),
+								borderRadius: adjustHeight(height, "0.625rem"),
+								textAlign: "center",
+								width: "30%",
+								height: adjustHeight(height, "25rem"),
+								fontFamily: fonts.textFont,
+								fontSize: fontSize.textSize,
+								display: "flex",
+								flexDirection: "column",
+								paddingLeft: adjustWidth(width, "4rem"),
+								paddingRight: adjustWidth(width, "4rem"),
+								justifyContent: "center",
+								alignContent: "center",
+								alignItems: "center",
+							}}
+						>
+							<h1
+								style={{
+									textAlign: "center",
+									fontFamily: fonts.titleFont,
+									color: colors.titleText,
+								}}
+							>
+								¡Verifica tu cuenta!
+							</h1>
+							<p
+								style={{
+									textAlign: "center",
+									fontSize: fontSize.textSize,
+									paddingTop: adjustHeight(height, "1rem"),
+									paddingBottom: adjustHeight(height, "2rem"),
+								}}
+							>
+								Hemos enviado un correo para confirmar tu usuario, por favor
+								revisa tu bandeja de entrada o spam.
+							</p>
+							<p>
+								En un momento serás redirigido a la página de inicio de sesión.
+							</p>
+						</div>
+					</div>
+				)}
+				<Child />
+			</div>
 		</Suspense>
 	);
 }
