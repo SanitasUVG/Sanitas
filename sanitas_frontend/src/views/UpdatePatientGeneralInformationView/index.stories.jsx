4520b51a3c86d2d5d4f2229becf1376d69f8e7c8
ElrohirGT <elrohirgt@gmail.com>
2024-10-03 19:51:24 -0600

    fix: General stories

--- sanitas_frontend/src/views/UpdatePatientGeneralInformationView/index.stories.jsx
+++ sanitas_frontend/src/views/UpdatePatientGeneralInformationView/index.stories.jsx
@@ -1,6 +1,7 @@
 import { MemoryRouter, Route, Routes } from "react-router-dom";
 import { createEmptyStore } from "src/store.mjs";
 import UpdateInfoView from "./index";
+import { STUDENT_DASHBOARD_SIDEBAR_PROPS } from "src/router";
 
 export default {
 	component: UpdateInfoView,
@@ -40,22 +41,6 @@ const examplePatientData = {
 
 const simulateNavigation = (path) => () =>
 	console.log(`Mock navigate to ${path}`);
-
-const sidebarConfigMock = {
-	navigateToGeneralStudent: simulateNavigation("/student-general"),
-	navigateToSurgicalStudent: simulateNavigation("/student-surgical"),
-	navigateToTraumatologicalStudent: simulateNavigation(
-		"/student-traumatological",
-	),
-	navigateToFamiliarStudent: simulateNavigation("/student-familiar"),
-	navigateToPersonalStudent: simulateNavigation("/student-personal"),
-	navigateToNonPathologicalStudent: simulateNavigation(
-		"/student-non-pathological",
-	),
-	navigateToAllergiesStudent: simulateNavigation("/student-allergies"),
-	navigateToPsiquiatricStudent: simulateNavigation("/student-psychiatric"),
-	navigateToObstetricsStudent: simulateNavigation("/student-obstetrics"),
-};
 
 const mockGetGeneralPatientInformation = async (id) => {
 	// await delay(2500)
@@ -105,7 +90,7 @@ export const WithPatientData = {
 		updateStudentPatientInformation: async (a) => ({ result: a }),
 		updateCollaboratorInformation: async (a) => ({ result: a }),
 		useStore: correctStore,
-		sidebarConfig: sidebarConfigMock,
+		sidebarConfig: STUDENT_DASHBOARD_SIDEBAR_PROPS,
 	},
 };
 
@@ -121,6 +106,6 @@ export const ErrorState = {
 		updateStudentPatientInformation: async () => ({ error: "MockError" }),
 		updateCollaboratorInformation: async () => ({ error: "MockError" }),
 		useStore: incorrectStore,
-		sidebarConfig: sidebarConfigMock,
+		sidebarConfig: STUDENT_DASHBOARD_SIDEBAR_PROPS,
 	},
 };
