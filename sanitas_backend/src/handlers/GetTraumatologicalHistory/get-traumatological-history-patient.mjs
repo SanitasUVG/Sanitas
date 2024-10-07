d4adfdbc16444a4589a69d6caed694225e42bc71
ElrohirGT <elrohirgt@gmail.com>
2024-10-07 11:02:11 -0600

    feat: Added protection to traumatological endpoint

--- sanitas_backend/src/handlers/GetTraumatologicalHistory/get-traumatological-history-patient.mjs
+++ sanitas_backend/src/handlers/GetTraumatologicalHistory/get-traumatological-history-patient.mjs
@@ -1,7 +1,8 @@
-import { getPgClient } from "db-conn";
+import { getPgClient, isDoctor, isEmailOfPatient } from "db-conn";
 import { logger, withRequest } from "logging";
 import { createResponse, mapToAPITraumatologicHistory } from "utils";
 import { genDefaultTraumatologicalHistory } from "utils/defaultValues.mjs";
+import { decodeJWT } from "utils/index.mjs";
 
 /**
  * Handles the HTTP GET request to retrieve traumatologic history for a specific patient by their ID.
@@ -24,6 +25,30 @@ export const getTraumatologicalHistoryHandler = async (event, context) => {
 			.build();
 	}
 
+	logger.info({ headers: event.headers }, "Received headers...");
+	const jwt = event.headers.Authorization;
+
+	logger.info({ jwt }, "Parsing JWT...");
+	const tokenInfo = decodeJWT(jwt);
+	if (tokenInfo.error) {
+		logger.error({ error: tokenInfo.error }, "JWT couldn't be parsed!");
+		return responseBuilder
+			.setStatusCode(400)
+			.setBody({ error: "JWT couldn't be parsed" })
+			.build();
+	}
+	const { email } = tokenInfo;
+	logger.info({ tokenInfo }, "JWT Parsed!");
+
+	const patientId = Number.parseInt(event.pathParameters?.id, 10);
+	if (Number.isNaN(patientId)) {
+		logger.error("Invalid ID received!", { id: event.pathParameters?.id });
+		return responseBuilder
+			.setStatusCode(400)
+			.setBody({ error: "Invalid request: No valid patientId supplied!" })
+			.build();
+	}
+
 	let client;
 	try {
 		const url = process.env.POSTGRES_URL;
@@ -32,18 +57,46 @@ export const getTraumatologicalHistoryHandler = async (event, context) => {
 		await client.connect();
 		logger.info("Connected!");
 
-		const id = Number.parseInt(event.pathParameters.id, 10);
-		if (!id) {
-			logger.error("Invalid ID received!");
-			return responseBuilder
-				.setStatusCode(400)
-				.setBody({ error: "Invalid request: No valid patientId supplied!" })
-				.build();
+		logger.info("Checking if user is doctor...");
+		const itsDoctor = await isDoctor(client, email);
+		if (itsDoctor.error) {
+			const msg =
+				"An error occurred while trying to check if the user is a doctor!";
+			logger.error(itsDoctor, msg);
+			return responseBuilder.setStatusCode(500).setBody(itsDoctor).build();
+		}
+
+		if (!itsDoctor) {
+			logger.info("User is patient!");
+			logger.info(
+				{ email, patientId },
+				"Checking if email belongs to patient id",
+			);
+			const emailBelongs = await isEmailOfPatient(client, email, patientId);
+
+			if (emailBelongs.error) {
+				const msg =
+					"An error ocurred while trying to check if the email belongs to the patient!";
+				logger.error(emailBelongs, msg);
+				return responseBuilder.setStatusCode(500).setBody(emailBelongs).build();
+			}
+
+			if (!emailBelongs) {
+				const msg = "The email doesn't belong to the patient id!";
+				logger.error({ email, patientId }, msg);
+				return responseBuilder
+					.setStatusCode(400)
+					.setBody({ error: msg })
+					.build();
+			}
+			logger.info("The email belongs to the patient!");
+		} else {
+			logger.info("The user is a doctor!");
 		}
 
 		const query =
 			"SELECT * FROM antecedentes_traumatologicos WHERE id_paciente = $1;";
-		const args = [id];
+		const args = [patientId];
 		logger.info({ query, args }, "Querying DB...");
 		const dbResponse = await client.query(query, args);
 		logger.info("Query done!");
@@ -55,7 +108,7 @@ export const getTraumatologicalHistoryHandler = async (event, context) => {
 			return responseBuilder
 				.setStatusCode(200)
 				.setBody({
-					patientId: id,
+					patientId: patientId,
 					medicalHistory: genDefaultTraumatologicalHistory(),
 				})
 				.build();
@@ -82,8 +135,6 @@ export const getTraumatologicalHistoryHandler = async (event, context) => {
 			})
 			.build();
 	} finally {
-		if (client) {
-			await client.end();
-		}
+		await client?.end();
 	}
 };
