3a7d9f9f4cdf014f39a61861f55f53fb07b97468
ElrohirGT <elrohirgt@gmail.com>
2024-10-10 08:56:48 -0600

    fix: GetGeneralStudentInfo endpoint

--- sanitas_backend/src/handlers/GetGeneralStudentInfo/get-general-student-info.mjs
+++ sanitas_backend/src/handlers/GetGeneralStudentInfo/get-general-student-info.mjs
@@ -1,4 +1,4 @@
-import { getPgClient, isDoctor, isEmailOfPatient } from "db-conn";
+import { getPgClient, isDoctor, isEmailOfPatient, transaction } from "db-conn";
 import { logger, withRequest } from "logging";
 import {
 	createResponse,
@@ -53,49 +53,67 @@ export const handler = async (event, context) => {
 		client = getPgClient(url);
 		await client.connect();
 
-		logger.info("Checking if user is doctor...");
-		const itsDoctor = await isDoctor(client, email);
-		if (itsDoctor.error) {
-			const msg =
-				"An error occurred while trying to check if the user is a doctor!";
-			logger.error(itsDoctor, msg);
-			return responseBuilder.setStatusCode(500).setBody(itsDoctor).build();
-		}
-
-		if (!itsDoctor) {
-			logger.info("User is patient!");
-			logger.info(
-				{ email, patientId },
-				"Checking if email belongs to patient id",
-			);
-			const emailBelongs = await isEmailOfPatient(client, email, patientId);
-
-			if (emailBelongs.error) {
+		const transactionResult = await transaction(client, logger, async () => {
+			logger.info("Checking if user is doctor...");
+			const itsDoctor = await isDoctor(client, email);
+			if (itsDoctor.error) {
 				const msg =
-					"An error ocurred while trying to check if the email belongs to the patient!";
-				logger.error(emailBelongs, msg);
-				return responseBuilder.setStatusCode(500).setBody(emailBelongs).build();
+					"An error occurred while trying to check if the user is a doctor!";
+				logger.error(itsDoctor, msg);
+				return responseBuilder.setStatusCode(500).setBody(itsDoctor).build();
 			}
 
-			if (!emailBelongs) {
-				const msg = "The email doesn't belong to the patient id!";
-				logger.error({ email, patientId }, msg);
-				return responseBuilder
-					.setStatusCode(400)
-					.setBody({ error: msg })
-					.build();
+			if (!itsDoctor) {
+				logger.info("User is patient!");
+				logger.info(
+					{ email, patientId },
+					"Checking if email belongs to patient id",
+				);
+				const emailBelongs = await isEmailOfPatient(client, email, patientId);
+
+				if (emailBelongs.error) {
+					const msg =
+						"An error ocurred while trying to check if the email belongs to the patient!";
+					logger.error(emailBelongs, msg);
+					return responseBuilder
+						.setStatusCode(500)
+						.setBody(emailBelongs)
+						.build();
+				}
+
+				if (!emailBelongs) {
+					const msg = "The email doesn't belong to the patient id!";
+					logger.error({ email, patientId }, msg);
+					return responseBuilder
+						.setStatusCode(400)
+						.setBody({ error: msg })
+						.build();
+				}
+				logger.info("The email belongs to the patient!");
+			} else {
+				logger.info("The user is a doctor!");
 			}
-			logger.info("The email belongs to the patient!");
-		} else {
-			logger.info("The user is a doctor!");
+
+			logger.info("Querying DB...");
+			const dbResponse = await client.query(
+				"SELECT * FROM estudiante WHERE ID_PACIENTE = $1 LIMIT 1;",
+				[patientId],
+			);
+
+			logger.info(dbResponse, "Query done!");
+			return dbResponse;
+		});
+
+		if (transactionResult.error) {
+			throw transactionResult.error;
 		}
 
-		logger.info("Querying DB...");
-		const dbResponse = await client.query(
-			"SELECT * FROM estudiante WHERE ID_PACIENTE = $1 LIMIT 1;",
-			[patientId],
-		);
-		logger.info(dbResponse, "Query done!");
+		if (transactionResult.response) {
+			logger.info(transactionResult, "Responding with:");
+			return transactionResult.response;
+		}
+
+		const { result: dbResponse } = transactionResult;
 
 		if (dbResponse.rowCount === 0) {
 			/** @type {import("utils/index.mjs").APIStudentInfo} */
@@ -115,12 +133,13 @@ export const handler = async (event, context) => {
 				.build();
 		}
 
-		logger.info("Creating response...");
-		logger.info(dbResponse.rows[0], "Responding with:");
-		return responseBuilder
+		const response = responseBuilder
 			.setStatusCode(200)
 			.setBody(mapToAPIStudentInfo(dbResponse.rows[0]))
 			.build();
+
+		logger.info({ response }, "Responding with:");
+		return response;
 	} catch (error) {
 		logger.error(error, "An error has occurred!");
 		return responseBuilder.setStatusCode(500).setBody(error).build();
