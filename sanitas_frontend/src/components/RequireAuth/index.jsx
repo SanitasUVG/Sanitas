c464fd2db84fca691aee8fde0d455838de0c942c
ElrohirGT <elrohirgt@gmail.com>
2024-10-17 19:05:20 -0600

    fix: pendejo bug no lee el código

--- sanitas_frontend/src/components/RequireAuth/index.jsx
+++ sanitas_frontend/src/components/RequireAuth/index.jsx
@@ -39,8 +39,6 @@ export default function RequireAuth({ children, getSession, path, useStore }) {
 		} else {
 			setIsRedirecting(false);
 			console.log("Session: ", session);
-			console.log("Session Token: ", session.getAccessToken());
-			console.log("payload: ", session.getAccessToken().payload);
 
 			setDisplayName(session.getIdToken.payload.email ?? "no-username-found");
 		}
