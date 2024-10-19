7c77e17a0bfc1a29fd1570a6db4360433e1c9621
ElrohirGT <elrohirgt@gmail.com>
2024-10-18 19:31:32 -0600

    fix: register view not working on mobile

--- sanitas_frontend/src/views/RegisterView/index.jsx
+++ sanitas_frontend/src/views/RegisterView/index.jsx
@@ -1,11 +1,10 @@
-import { Suspense, useState } from "react";
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import logoSanitas from "src/assets/images/logoSanitas.png";
 import backgroundImage from "src/assets/images/UVGBackground.jpg";
 import uvgLogo from "src/assets/images/uvgLogo.jpg";
 import BaseButton from "src/components/Button/Base";
 import { BaseInput } from "src/components/Input";
-import Throbber from "src/components/Throbber";
 import { NAV_PATHS } from "src/router";
 import { colors, fonts, fontSize } from "src/theme.mjs";
 import WrapPromise from "src/utils/promiseWrapper";
@@ -20,6 +19,7 @@ import useWindowSize from "src/utils/useWindowSize";
 /**
  * @param {RegisterViewProps} props
  */
+// biome-ignore lint/complexity/noExcessiveCognitiveComplexity: Ignoring complexity for this function
 export default function RegisterView({ registerUser }) {
 	/** @type React.CSSStyleDeclaration */
 	const inputStyles = {
@@ -33,56 +33,115 @@ export default function RegisterView({ registerUser }) {
 
 	const [showPopup, setShowPopup] = useState(false);
 
-	// biome-ignore lint/complexity/noExcessiveCognitiveComplexity: Ignoring complexity for this function
-	const Child = () => {
-		const navigate = useNavigate();
-		const [username, setUsername] = useState("");
-		const [password, setPassword] = useState("");
-		const [errorMessage, setErrorMessage] = useState("");
-		/** @type {[import("src/utils/promiseWrapper").SuspenseResource<import("src/dataLayer.mjs").Result<any, any>>, Function]} */
-		const [registerResource, setRegisterResource] = useState(null);
+	const navigate = useNavigate();
+	const [username, setUsername] = useState("");
+	const [password, setPassword] = useState("");
+	const [errorMessage, setErrorMessage] = useState("");
+	/** @type {[import("src/utils/promiseWrapper").SuspenseResource<import("src/dataLayer.mjs").Result<any, any>>, Function]} */
+	const [registerResource, setRegisterResource] = useState(null);
 
-		const handleRegister = () => {
-			if (!(username && password)) {
-				setErrorMessage("Por favor, complete todos los campos.");
-				return;
-			}
-			setRegisterResource(WrapPromise(registerUser(username, password)));
-		};
-
-		if (registerResource !== null) {
-			const response = registerResource.read();
-			if (!response.error) {
-				setShowPopup(true); // Mostrar el popup
-				setTimeout(() => {
-					navigate(NAV_PATHS.LOGIN_USER, { replace: true });
-				}, 7000);
-			} else {
-				console.log(response);
-				const errorType = response.error.code;
-				console.log(errorType);
-				switch (errorType) {
-					case "UsernameExistsException":
-						setErrorMessage(
-							"El usuario ya está registrado. Intente con otro correo.",
-						);
-						break;
-					case "InvalidParameterException":
-						setErrorMessage("Revise el correo o la contraseña por favor.");
-						break;
-					case "InvalidPasswordException":
-						setErrorMessage(
-							"La contraseña es muy débil. Intente con una más segura.",
-						);
-						break;
-					default:
-						setErrorMessage("Lo sentimos! Ha ocurrido un error interno.");
-				}
-				setRegisterResource(null);
-			}
+	const handleRegister = () => {
+		if (!(username && password)) {
+			setErrorMessage("Por favor, complete todos los campos.");
+			return;
 		}
+		setRegisterResource(WrapPromise(registerUser(username, password)));
+	};
 
-		return (
+	if (registerResource !== null) {
+		const response = registerResource.read();
+		if (!response.error) {
+			setShowPopup(true); // Mostrar el popup
+			setTimeout(() => {
+				navigate(NAV_PATHS.LOGIN_USER, { replace: true });
+			}, 7000);
+		} else {
+			console.log(response);
+			const errorType = response.error.code;
+			console.log(errorType);
+			switch (errorType) {
+				case "UsernameExistsException":
+					setErrorMessage(
+						"El usuario ya está registrado. Intente con otro correo.",
+					);
+					break;
+				case "InvalidParameterException":
+					setErrorMessage("Revise el correo o la contraseña por favor.");
+					break;
+				case "InvalidPasswordException":
+					setErrorMessage(
+						"La contraseña es muy débil. Intente con una más segura.",
+					);
+					break;
+				default:
+					setErrorMessage("Lo sentimos! Ha ocurrido un error interno.");
+			}
+			setRegisterResource(null);
+		}
+	}
+
+	return (
+		<>
+			{showPopup && (
+				<div
+					style={{
+						position: "fixed",
+						top: 0,
+						left: 0,
+						width: "100%",
+						height: "100%",
+						backgroundColor: "rgba(0,0,0,0.5)",
+						display: "flex",
+						justifyContent: "center",
+						alignItems: "center",
+						zIndex: 1000,
+					}}
+				>
+					<div
+						style={{
+							backgroundColor: "white",
+							padding: adjustWidth(width, "1.25rem"),
+							borderRadius: adjustHeight(height, "0.625rem"),
+							textAlign: "center",
+							width: "30%",
+							height: adjustHeight(height, "25rem"),
+							fontFamily: fonts.textFont,
+							fontSize: fontSize.textSize,
+							display: "flex",
+							flexDirection: "column",
+							paddingLeft: adjustWidth(width, "4rem"),
+							paddingRight: adjustWidth(width, "4rem"),
+							justifyContent: "center",
+							alignContent: "center",
+							alignItems: "center",
+						}}
+					>
+						<h1
+							style={{
+								textAlign: "center",
+								fontFamily: fonts.titleFont,
+								color: colors.titleText,
+							}}
+						>
+							¡Verifica tu cuenta!
+						</h1>
+						<p
+							style={{
+								textAlign: "center",
+								fontSize: isMobile ? "0.75rem" : fontSize.textSize,
+								paddingTop: adjustHeight(height, "1rem"),
+								paddingBottom: adjustHeight(height, "2rem"),
+							}}
+						>
+							Hemos enviado un correo para confirmar tu usuario, por favor
+							revisa tu bandeja de entrada o spam.
+						</p>
+						<p>
+							En un momento serás redirigido a la página de inicio de sesión.
+						</p>
+					</div>
+				</div>
+			)}
 			<div
 				style={{
 					backgroundImage: `url(${backgroundImage})`,
@@ -268,97 +327,6 @@ export default function RegisterView({ registerUser }) {
 					)}
 				</div>
 			</div>
-		);
-	};
-
-	const LoadingView = () => {
-		return (
-			<div
-				style={{
-					height: "100vh",
-				}}
-			>
-				<Throbber loadingMessage="Registrando usuario..." />
-			</div>
-		);
-	};
-
-	return (
-		<Suspense fallback={<LoadingView />}>
-			<div
-				style={{
-					backgroundImage: `url(${backgroundImage})`,
-					backgroundColor: "black",
-					backgroundSize: "cover",
-					width: "100vw",
-					height: "100vh",
-					display: "grid",
-					alignItems: "center",
-					justifyItems: "center",
-				}}
-			>
-				{showPopup && (
-					<div
-						style={{
-							position: "fixed",
-							top: 0,
-							left: 0,
-							width: "100%",
-							height: "100%",
-							backgroundColor: "rgba(0,0,0,0.5)",
-							display: "flex",
-							justifyContent: "center",
-							alignItems: "center",
-							zIndex: 1000,
-						}}
-					>
-						<div
-							style={{
-								backgroundColor: "white",
-								padding: adjustWidth(width, "1.25rem"),
-								borderRadius: adjustHeight(height, "0.625rem"),
-								textAlign: "center",
-								width: "30%",
-								height: adjustHeight(height, "25rem"),
-								fontFamily: fonts.textFont,
-								fontSize: fontSize.textSize,
-								display: "flex",
-								flexDirection: "column",
-								paddingLeft: adjustWidth(width, "4rem"),
-								paddingRight: adjustWidth(width, "4rem"),
-								justifyContent: "center",
-								alignContent: "center",
-								alignItems: "center",
-							}}
-						>
-							<h1
-								style={{
-									textAlign: "center",
-									fontFamily: fonts.titleFont,
-									color: colors.titleText,
-								}}
-							>
-								¡Verifica tu cuenta!
-							</h1>
-							<p
-								style={{
-									textAlign: "center",
-									fontSize: isMobile ? "0.75rem" : fontSize.textSize,
-									paddingTop: adjustHeight(height, "1rem"),
-									paddingBottom: adjustHeight(height, "2rem"),
-								}}
-							>
-								Hemos enviado un correo para confirmar tu usuario, por favor
-								revisa tu bandeja de entrada o spam.
-							</p>
-							<p>
-								En un momento serás redirigido a la página de inicio de sesión.
-							</p>
-						</div>
-					</div>
-				)}
-				<Child />
-			</div>
-		</Suspense>
+		</>
 	);
 }
