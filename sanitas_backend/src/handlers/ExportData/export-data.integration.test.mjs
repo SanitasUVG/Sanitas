2afaba3fbf9d874d4311b557c6de29e886587d55
ElrohirGT <elrohirgt@gmail.com>
2024-10-20 21:43:00 -0600

    fix: linter suggestions

---
+++ sanitas_backend/src/handlers/ExportData/export-data.integration.test.mjs
@@ -0,0 +1,21 @@
+import { beforeAll, describe, expect, test } from "@jest/globals";
+import axios from "axios";
+import {
+	createAuthorizationHeader,
+	createDoctorJWT,
+	createInvalidJWT,
+	createPatientJWT,
+	createTestPatient,
+	LOCAL_API_URL,
+} from "../testHelpers.mjs";
+
+const API_URL = `${LOCAL_API_URL}consultations/export`;
+
+describe("Get consultations data endpoint", () => {
+	const validHeaders = createAuthorizationHeader(createDoctorJWT());
+	let patientId;
+
+	beforeAll(async () => {
+		patientId = await createTestPatient();
+	});
+});
