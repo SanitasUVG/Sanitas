ef2068a1d38d926e6ac1ab9029d38f8dc505f242
ElrohirGT <elrohirgt@gmail.com>
2024-10-24 20:00:45 -0600

    feat: Added md_san SCHEMA

--- database/tables.sql
+++ database/tables.sql
@@ -1,278 +1,274 @@
-CREATE TABLE USUARIO (
-    EMAIL VARCHAR(50) PRIMARY KEY NOT NULL,
-    TIPO VARCHAR(20) NOT NULL
+DROP TABLE IF EXISTS md_san.paciente;
+CREATE TABLE md_san.paciente (
+    id SERIAL PRIMARY KEY,
+    cui VARCHAR(24) UNIQUE NOT NULL,
+    correo VARCHAR(50),
+    es_mujer BOOLEAN NOT NULL,
+    nombres VARCHAR(50) NOT NULL,
+    apellidos VARCHAR(50) NOT NULL,
+    nombre_contacto1 VARCHAR(50),
+    parentesco_contacto1 VARCHAR(24),
+    telefono_contacto1 VARCHAR(20),
+    nombre_contacto2 VARCHAR(50),
+    parentesco_contacto2 VARCHAR(24),
+    telefono_contacto2 VARCHAR(20),
+    tipo_sangre VARCHAR(3),
+    direccion VARCHAR(100),
+    seguro VARCHAR(30),
+    fecha_nacimiento DATE NOT NULL,
+    telefono VARCHAR(20)
 );
-
-CREATE TABLE PACIENTE (
-    ID SERIAL PRIMARY KEY,
-    CUI VARCHAR(24) UNIQUE NOT NULL,
-    CORREO VARCHAR(50),
-    ES_MUJER BOOLEAN NOT NULL,
-    NOMBRES VARCHAR(50) NOT NULL,
-    APELLIDOS VARCHAR(50) NOT NULL,
-    NOMBRE_CONTACTO1 VARCHAR(50),
-    PARENTESCO_CONTACTO1 VARCHAR(24),
-    TELEFONO_CONTACTO1 VARCHAR(20),
-    NOMBRE_CONTACTO2 VARCHAR(50),
-    PARENTESCO_CONTACTO2 VARCHAR(24),
-    TELEFONO_CONTACTO2 VARCHAR(20),
-    TIPO_SANGRE VARCHAR(3),
-    DIRECCION VARCHAR(100),
-    SEGURO VARCHAR(30),
-    FECHA_NACIMIENTO DATE NOT NULL,
-    TELEFONO VARCHAR(20)
-);
-CREATE UNIQUE INDEX PACIENTE_CUI_IDX ON PACIENTE (CUI);
-COMMENT ON TABLE PACIENTE IS
+DROP INDEX IF EXISTS paciente_cui_idx;
+CREATE UNIQUE INDEX paciente_cui_idx ON md_san.paciente (cui);
+COMMENT ON TABLE md_san.paciente IS
 'This table is used to save general information about a patient.';
 
-CREATE TABLE CUENTA_PACIENTE (
-    EMAIL VARCHAR(50) PRIMARY KEY,
-    CUI_PACIENTE VARCHAR(24) NOT NULL UNIQUE REFERENCES PACIENTE (CUI)
+DROP TABLE IF EXISTS md_san.cuenta_paciente;
+CREATE TABLE md_san.cuenta_paciente (
+    email VARCHAR(50) PRIMARY KEY,
+    cui_paciente VARCHAR(24) NOT NULL UNIQUE REFERENCES paciente (cui)
 );
-COMMENT ON TABLE CUENTA_PACIENTE IS
+COMMENT ON TABLE md_san.cuenta_paciente IS
 'Used to save the relation between a cognito account and a patient';
 
-CREATE TABLE DOCTOR (
-    EMAIL VARCHAR(100) NOT NULL PRIMARY KEY
+DROP TABLE IF EXISTS md_san.doctor;
+CREATE TABLE md_san.doctor (
+    email VARCHAR(100) NOT NULL PRIMARY KEY
 );
-COMMENT ON TABLE DOCTOR IS
+COMMENT ON TABLE md_san.doctor IS
 'Used for saving all user emails that have the role of a doctor.';
 
-
-CREATE TABLE ESTUDIANTE (
-    CARNET VARCHAR(20) PRIMARY KEY NOT NULL,
-    CARRERA VARCHAR(50),
-    ID_PACIENTE INTEGER NOT NULL UNIQUE,
-    FOREIGN KEY (ID_PACIENTE) REFERENCES PACIENTE (ID)
+DROP TABLE IF EXISTS md_san.estudiante;
+CREATE TABLE md_san.estudiante (
+    carnet VARCHAR(20) PRIMARY KEY NOT NULL,
+    carrera VARCHAR(50),
+    id_paciente INTEGER NOT NULL UNIQUE,
+    FOREIGN KEY (id_paciente) REFERENCES md_san.paciente (id)
 );
 
-CREATE TABLE COLABORADOR (
-    CODIGO VARCHAR(20) PRIMARY KEY NOT NULL,
-    AREA VARCHAR(50) NOT NULL,
-    ID_PACIENTE INTEGER NOT NULL UNIQUE,
-    FOREIGN KEY (ID_PACIENTE) REFERENCES PACIENTE (ID)
+DROP TABLE IF EXISTS md_san.colaborador;
+CREATE TABLE md_san.colaborador (
+    codigo VARCHAR(20) PRIMARY KEY NOT NULL,
+    area VARCHAR(50) NOT NULL,
+    id_paciente INTEGER NOT NULL UNIQUE,
+    FOREIGN KEY (id_paciente) REFERENCES md_san.paciente (id)
 );
 
-CREATE TABLE CONSULTA (
-    ID SERIAL PRIMARY KEY NOT NULL,
-    ID_PACIENTE INTEGER NOT NULL,
-    FECHA TIMESTAMP NOT NULL,
-    MOTIVO TEXT NOT NULL,
-    DIAGNOSTICO TEXT NOT NULL,
-    EXAMEN_FISICO TEXT NOT NULL,
-    FRECUENCIA_RESPIRATORIA DOUBLE PRECISION,
-    TEMPERATURA DOUBLE PRECISION,
-    SATURACION_OXIGENO DOUBLE PRECISION,
-    GLUCOMETRIA DOUBLE PRECISION,
-    FRECUENCIA_CARDIACA DOUBLE PRECISION,
-    PRESION_SISTOLICA INTEGER,
-    PRESION_DIASTOLICA INTEGER,
-    EVALUADOR VARCHAR(50) NOT NULL,
-    MEDICAMENTOS_DATA JSON,
-    NOTAS TEXT,
-    FOREIGN KEY (ID_PACIENTE) REFERENCES PACIENTE (ID),
-    FOREIGN KEY (EVALUADOR) REFERENCES USUARIO (EMAIL)
+DROP TABLE IF EXISTS md_san.consulta;
+CREATE TABLE md_san.consulta (
+    id SERIAL PRIMARY KEY NOT NULL,
+    id_paciente INTEGER NOT NULL,
+    fecha TIMESTAMP NOT NULL,
+    motivo TEXT NOT NULL,
+    diagnostico TEXT NOT NULL,
+    examen_fisico TEXT NOT NULL,
+    frecuencia_respiratoria DOUBLE PRECISION,
+    temperatura DOUBLE PRECISION,
+    saturacion_oxigeno DOUBLE PRECISION,
+    glucometria DOUBLE PRECISION,
+    frecuencia_cardiaca DOUBLE PRECISION,
+    presion_sistolica INTEGER,
+    presion_diastolica INTEGER,
+    evaluador VARCHAR(50) NOT NULL,
+    medicamentos_data JSON,
+    notas TEXT,
+    FOREIGN KEY (id_paciente) REFERENCES md_san.paciente (id),
+    FOREIGN KEY (evaluador) REFERENCES md_san.usuario (email)
 );
 
-CREATE TABLE DIAGNOSTICO (
-    ID SERIAL PRIMARY KEY NOT NULL,
-    ID_CONSULTA INTEGER NOT NULL,
-    NOMBRE VARCHAR(50) NOT NULL,
-    TRATAMIENTO TEXT,
-    FOREIGN KEY (ID_CONSULTA) REFERENCES CONSULTA (ID)
+DROP TABLE IF EXISTS md_san.diagnostico;
+CREATE TABLE md_san.diagnostico (
+    id SERIAL PRIMARY KEY NOT NULL,
+    id_consulta INTEGER NOT NULL,
+    nombre VARCHAR(50) NOT NULL,
+    tratamiento TEXT,
+    FOREIGN KEY (id_consulta) REFERENCES md_san.consulta (id)
 );
 
-CREATE TABLE MEDICAMENTO (
-    ID SERIAL PRIMARY KEY NOT NULL,
-    NOMBRE VARCHAR(50) NOT NULL,
-    CANTIDAD INTEGER,
-    ID_CONSULTA INTEGER NOT NULL,
-    FOREIGN KEY (ID_CONSULTA) REFERENCES CONSULTA (ID)
+DROP TABLE IF EXISTS md_san.medicamento;
+CREATE TABLE md_san.medicamento (
+    id SERIAL PRIMARY KEY NOT NULL,
+    nombre VARCHAR(50) NOT NULL,
+    cantidad INTEGER,
+    id_consulta INTEGER NOT NULL,
+    FOREIGN KEY (id_consulta) REFERENCES md_san.consulta (id)
 );
 
-CREATE TABLE ANTECEDENTES_FAMILIARES (
-    ID_PACIENTE INTEGER PRIMARY KEY NOT NULL,
-    HIPERTENSION_ARTERIAL BOOLEAN,
-    HIPERTENSION_ARTERIAL_DATA JSON,
-    DIABETES_MELLITUS BOOLEAN,
-    DIABETES_MELLITUS_DATA JSON,
-    HIPOTIROIDISMO BOOLEAN,
-    HIPOTIROIDISMO_DATA JSON,
-    ASMA BOOLEAN,
-    ASMA_DATA JSON,
-    CONVULSIONES BOOLEAN,
-    CONVULSIONES_DATA JSON,
-    INFARTO_AGUDO_MIOCARDIO BOOLEAN,
-    INFARTO_AGUDO_MIOCARDIO_DATA JSON,
-    CANCER BOOLEAN,
-    CANCER_DATA JSON,
-    ENFERMEDADES_CARDIACAS BOOLEAN,
-    ENFERMEDADES_CARDIACAS_DATA JSON,
-    ENFERMEDADES_RENALES BOOLEAN,
-    ENFERMEDADES_RENALES_DATA JSON,
-    OTROS BOOLEAN,
-    OTROS_DATA JSON,
-    FOREIGN KEY (ID_PACIENTE) REFERENCES PACIENTE (ID)
+DROP TABLE IF EXISTS md_san.antecedentes_familiares;
+CREATE TABLE md_san.antecedentes_familiares (
+    id_paciente INTEGER PRIMARY KEY NOT NULL,
+    hipertension_arterial BOOLEAN,
+    hipertension_arterial_data JSON,
+    diabetes_mellitus BOOLEAN,
+    diabetes_mellitus_data JSON,
+    hipotiroidismo BOOLEAN,
+    hipotiroidismo_data JSON,
+    asma BOOLEAN,
+    asma_data JSON,
+    convulsiones BOOLEAN,
+    convulsiones_data JSON,
+    infarto_agudo_miocardio BOOLEAN,
+    infarto_agudo_miocardio_data JSON,
+    cancer BOOLEAN,
+    cancer_data JSON,
+    enfermedades_cardiacas BOOLEAN,
+    enfermedades_cardiacas_data JSON,
+    enfermedades_renales BOOLEAN,
+    enfermedades_renales_data JSON,
+    otros BOOLEAN,
+    otros_data JSON,
+    FOREIGN KEY (id_paciente) REFERENCES md_san.paciente (id)
 );
 
-CREATE TABLE ANTECEDENTES_PERSONALES (
-    ID_PACIENTE INTEGER PRIMARY KEY NOT NULL,
-    HIPERTENSION_ARTERIAL BOOLEAN,
-    HIPERTENSION_ARTERIAL_DATA JSON,
-    DIABETES_MELLITUS BOOLEAN,
-    DIABETES_MELLITUS_DATA JSON,
-    HIPOTIROIDISMO BOOLEAN,
-    HIPOTIROIDISMO_DATA JSON,
-    ASMA BOOLEAN,
-    ASMA_DATA JSON,
-    CONVULSIONES BOOLEAN,
-    CONVULSIONES_DATA JSON,
-    INFARTO_AGUDO_MIOCARDIO BOOLEAN,
-    INFARTO_AGUDO_MIOCARDIO_DATA JSON,
-    CANCER BOOLEAN,
-    CANCER_DATA JSON,
-    ENFERMEDADES_CARDIACAS BOOLEAN,
-    ENFERMEDADES_CARDIACAS_DATA JSON,
-    ENFERMEDADES_RENALES BOOLEAN,
-    ENFERMEDADES_RENALES_DATA JSON,
-    OTROS BOOLEAN,
-    OTROS_DATA JSON,
-    FOREIGN KEY (ID_PACIENTE) REFERENCES PACIENTE (ID)
+DROP TABLE IF EXISTS md_san.antecedentes_personales;
+CREATE TABLE md_san.antecedentes_personales (
+    id_paciente INTEGER PRIMARY KEY NOT NULL,
+    hipertension_arterial BOOLEAN,
+    hipertension_arterial_data JSON,
+    diabetes_mellitus BOOLEAN,
+    diabetes_mellitus_data JSON,
+    hipotiroidismo BOOLEAN,
+    hipotiroidismo_data JSON,
+    asma BOOLEAN,
+    asma_data JSON,
+    convulsiones BOOLEAN,
+    convulsiones_data JSON,
+    infarto_agudo_miocardio BOOLEAN,
+    infarto_agudo_miocardio_data JSON,
+    cancer BOOLEAN,
+    cancer_data JSON,
+    enfermedades_cardiacas BOOLEAN,
+    enfermedades_cardiacas_data JSON,
+    enfermedades_renales BOOLEAN,
+    enfermedades_renales_data JSON,
+    otros BOOLEAN,
+    otros_data JSON,
+    FOREIGN KEY (id_paciente) REFERENCES md_san.paciente (id)
 );
 
-CREATE TABLE ANTECEDENTES_ALERGICOS (
-    ID_PACIENTE INTEGER PRIMARY KEY NOT NULL,
-    MEDICAMENTO BOOLEAN,
-    MEDICAMENTO_DATA JSON,
-    COMIDA BOOLEAN,
-    COMIDA_DATA JSON,
-    POLVO BOOLEAN,
-    POLVO_DATA JSON,
-    POLEN BOOLEAN,
-    POLEN_DATA JSON,
-    CAMBIO_DE_CLIMA BOOLEAN,
-    CAMBIO_DE_CLIMA_DATA JSON,
-    ANIMALES BOOLEAN,
-    ANIMALES_DATA JSON,
-    OTROS BOOLEAN,
-    OTROS_DATA JSON,
-    FOREIGN KEY (ID_PACIENTE) REFERENCES PACIENTE (ID)
+DROP TABLE IF EXISTS md_san.antecedentes_alergicos;
+CREATE TABLE md_san.antecedentes_alergicos (
+    id_paciente INTEGER PRIMARY KEY NOT NULL,
+    medicamento BOOLEAN,
+    medicamento_data JSON,
+    comida BOOLEAN,
+    comida_data JSON,
+    polvo BOOLEAN,
+    polvo_data JSON,
+    polen BOOLEAN,
+    polen_data JSON,
+    cambio_de_clima BOOLEAN,
+    cambio_de_clima_data JSON,
+    animales BOOLEAN,
+    animales_data JSON,
+    otros BOOLEAN,
+    otros_data JSON,
+    FOREIGN KEY (id_paciente) REFERENCES md_san.paciente (id)
 );
 
-CREATE TABLE ANTECEDENTES_QUIRURGICOS (
-    ID_PACIENTE INTEGER PRIMARY KEY NOT NULL,
-    ANTECEDENTE_QUIRURGICO BOOLEAN,
-    ANTECEDENTE_QUIRURGICO_DATA JSON,
-    FOREIGN KEY (ID_PACIENTE) REFERENCES PACIENTE (ID)
+DROP TABLE IF EXISTS md_san.antecedentes_quirurgicos;
+CREATE TABLE md_san.antecedentes_quirurgicos (
+    id_paciente INTEGER PRIMARY KEY NOT NULL,
+    antecedente_quirurgico BOOLEAN,
+    antecedente_quirurgico_data JSON,
+    FOREIGN KEY (id_paciente) REFERENCES md_san.paciente (id)
 );
 
-CREATE TABLE ANTECEDENTES_TRAUMATOLOGICOS (
-    ID_PACIENTE INTEGER PRIMARY KEY NOT NULL,
-    ANTECEDENTE_TRAUMATOLOGICO BOOLEAN,
-    ANTECEDENTE_TRAUMATOLOGICO_DATA JSON,
-    FOREIGN KEY (ID_PACIENTE) REFERENCES PACIENTE (ID)
+DROP TABLE IF EXISTS md_san.antecedentes_traumatologicos;
+CREATE TABLE md_san.antecedentes_traumatologicos (
+    id_paciente INTEGER PRIMARY KEY NOT NULL,
+    antecedente_traumatologico BOOLEAN,
+    antecedente_traumatologico_data JSON,
+    FOREIGN KEY (id_paciente) REFERENCES md_san.paciente (id)
 );
 
-CREATE TABLE ANTECEDENTES_PSIQUIATRICOS (
-    ID_PACIENTE INTEGER PRIMARY KEY NOT NULL,
-    DEPRESION BOOLEAN,
-    DEPRESION_DATA JSON,
-    ANSIEDAD BOOLEAN,
-    ANSIEDAD_DATA JSON,
-    TOC BOOLEAN,
-    TOC_DATA JSON,
-    TDAH BOOLEAN,
-    TDAH_DATA JSON,
-    BIPOLARIDAD BOOLEAN,
-    BIPOLARIDAD_DATA JSON,
-    OTRO BOOLEAN,
-    OTRO_DATA JSON,
-    FOREIGN KEY (ID_PACIENTE) REFERENCES PACIENTE (ID)
+DROP TABLE IF EXISTS md_san.antecedentes_psiquiatricos;
+CREATE TABLE md_san.antecedentes_psiquiatricos (
+    id_paciente INTEGER PRIMARY KEY NOT NULL,
+    depresion BOOLEAN,
+    depresion_data JSON,
+    ansiedad BOOLEAN,
+    ansiedad_data JSON,
+    toc BOOLEAN,
+    toc_data JSON,
+    tdah BOOLEAN,
+    tdah_data JSON,
+    bipolaridad BOOLEAN,
+    bipolaridad_data JSON,
+    otro BOOLEAN,
+    otro_data JSON,
+    FOREIGN KEY (id_paciente) REFERENCES md_san.paciente (id)
 );
 
-CREATE TABLE ANTECEDENTES_GINECOOBSTETRICOS (
-    ID_PACIENTE INTEGER PRIMARY KEY NOT NULL,
-    EDAD_PRIMERA_MENSTRUACION BOOLEAN,
-    EDAD_PRIMERA_MENSTRUACION_DATA JSON,
-    CICLOS_REGULARES BOOLEAN,
-    CICLOS_REGULARES_DATA JSON,
-    MENSTRUACION_DOLOROSA BOOLEAN,
-    MENSTRUACION_DOLOROSA_DATA JSON,
-    NUM_EMBARAZOS INTEGER,
-    NUM_PARTOS INTEGER,
-    NUM_CESAREAS INTEGER,
-    NUM_ABORTOS INTEGER,
-    MEDICACION_QUISTES_OVARICOS BOOLEAN,
-    MEDICACION_QUISTES_OVARICOS_DATA JSON,
-    MEDICACION_MIOMATOSIS BOOLEAN,
-    MEDICACION_MIOMATOSIS_DATA JSON,
-    MEDICACION_ENDOMETRIOSIS BOOLEAN,
-    MEDICACION_ENDOMETRIOSIS_DATA JSON,
-    MEDICACION_OTRA_CONDICION BOOLEAN,
-    MEDICACION_OTRA_CONDICION_DATA JSON,
-    CIRUGIA_QUISTES_OVARICOS BOOLEAN,
-    CIRUGIA_QUISTES_OVARICOS_DATA JSON,
-    CIRUGIA_HISTERECTOMIA BOOLEAN,
-    CIRUGIA_HISTERECTOMIA_DATA JSON,
-    CIRUGIA_ESTERILIZACION BOOLEAN,
-    CIRUGIA_ESTERILIZACION_DATA JSON,
-    CIRUGIA_RESECCION_MASAS BOOLEAN,
-    CIRUGIA_RESECCION_MASAS_DATA JSON,
-    FOREIGN KEY (ID_PACIENTE) REFERENCES PACIENTE (ID)
+DROP TABLE IF EXISTS md_san.antecedentes_ginecoobstetricos;
+CREATE TABLE md_san.antecedentes_ginecoobstetricos (
+    id_paciente INTEGER PRIMARY KEY NOT NULL,
+    edad_primera_menstruacion BOOLEAN,
+    edad_primera_menstruacion_data JSON,
+    ciclos_regulares BOOLEAN,
+    ciclos_regulares_data JSON,
+    menstruacion_dolorosa BOOLEAN,
+    menstruacion_dolorosa_data JSON,
+    num_embarazos INTEGER,
+    num_partos INTEGER,
+    num_cesareas INTEGER,
+    num_abortos INTEGER,
+    medicacion_quistes_ovaricos BOOLEAN,
+    medicacion_quistes_ovaricos_data JSON,
+    medicacion_miomatosis BOOLEAN,
+    medicacion_miomatosis_data JSON,
+    medicacion_endometriosis BOOLEAN,
+    medicacion_endometriosis_data JSON,
+    medicacion_otra_condicion BOOLEAN,
+    medicacion_otra_condicion_data JSON,
+    cirugia_quistes_ovaricos BOOLEAN,
+    cirugia_quistes_ovaricos_data JSON,
+    cirugia_histerectomia BOOLEAN,
+    cirugia_histerectomia_data JSON,
+    cirugia_esterilizacion BOOLEAN,
+    cirugia_esterilizacion_data JSON,
+    cirugia_reseccion_masas BOOLEAN,
+    cirugia_reseccion_masas_data JSON,
+    FOREIGN KEY (id_paciente) REFERENCES md_san.paciente (id)
 );
 
-CREATE TABLE ANTECEDENTES_NO_PATOLOGICOS (
-    ID_PACIENTE INTEGER PRIMARY KEY NOT NULL,
-    TIPO_SANGRE VARCHAR(3) NOT NULL,
-    FUMA BOOLEAN,
-    FUMA_DATA JSON,
-    BEBIDAS_ALCOHOLICAS BOOLEAN,
-    BEBIDAS_ALCOHOLICAS_DATA JSON,
-    DROGAS BOOLEAN,
-    DROGAS_DATA JSON,
-    FOREIGN KEY (ID_PACIENTE) REFERENCES PACIENTE (ID)
+DROP TABLE IF EXISTS md_san.antecedentes_no_patologicos;
+CREATE TABLE md_san.antecedentes_no_patologicos (
+    id_paciente INTEGER PRIMARY KEY NOT NULL,
+    tipo_sangre VARCHAR(3) NOT NULL,
+    fuma BOOLEAN,
+    fuma_data JSON,
+    bebidas_alcoholicas BOOLEAN,
+    bebidas_alcoholicas_data JSON,
+    drogas BOOLEAN,
+    drogas_data JSON,
+    FOREIGN KEY (id_paciente) REFERENCES md_san.paciente (id)
 );
 
-CREATE TABLE SESION (
-    TOKEN VARCHAR(100) PRIMARY KEY NOT NULL,
-    CREATED TIMESTAMP NOT NULL,
-    EMAIL VARCHAR(50) NOT NULL,
-    FOREIGN KEY (EMAIL) REFERENCES USUARIO (EMAIL)
-);
-
-CREATE TABLE BITACORA (
-    FECHA TIMESTAMP NOT NULL,
-    USUARIO VARCHAR(50) NOT NULL,
-    ACCION VARCHAR(50) NOT NULL,
-    TABLA VARCHAR(50) NOT NULL,
-    ID_REGISTRO VARCHAR(50) NOT NULL
-);
-
-CREATE MATERIALIZED VIEW STATS
+CREATE MATERIALIZED VIEW md_san.stats
 AS
 SELECT
-    P.ID,
-    P.FECHA_NACIMIENTO,
-    C.FECHA AS FECHA_VISITA,
-    C.DIAGNOSTICO,
-    ES.CARRERA,
+    p.id,
+    p.fecha_nacimiento,
+    c.fecha AS fecha_visita,
+    c.diagnostico,
+    es.carrera,
     CASE
-        WHEN P.ES_MUJER = true THEN 'F'
+        WHEN p.es_mujer = true THEN 'F'
         ELSE 'M'
-    END AS SEXO,
-    COALESCE(ES.ID_PACIENTE, 0) AS ES_ESTUDIANTE,
-    COALESCE(CO.ID_PACIENTE, 0) AS ES_COLABORADOR,
+    END AS sexo,
+    COALESCE(es.id_paciente, 0) AS es_estudiante,
+    COALESCE(co.id_paciente, 0) AS es_colaborador,
     COALESCE(
-        ES.ID_PACIENTE IS null AND CO.ID_PACIENTE IS null, false
-    ) AS ES_VISITA
+        es.id_paciente IS null AND co.id_paciente IS null, false
+    ) AS es_visita
 FROM
-    CONSULTA AS C
-INNER JOIN PACIENTE AS P
+    md_san.consulta AS c
+INNER JOIN md_san.paciente AS p
     ON
-        C.ID_PACIENTE = P.ID
-LEFT JOIN ESTUDIANTE AS ES
+        c.id_paciente = p.id
+LEFT JOIN md_san.estudiante AS es
     ON
-        C.ID_PACIENTE = ES.ID_PACIENTE
-LEFT JOIN COLABORADOR AS CO ON
-    C.ID_PACIENTE = CO.ID_PACIENTE;
+        c.id_paciente = es.id_paciente
+LEFT JOIN md_san.colaborador AS co ON
+    c.id_paciente = co.id_paciente;
